3a79eb85fae1bf5ce2a6b7b5c91841b5f5233549
Austin <austin@flipsidecrypto.com>
2024-09-12 00:30:13 -0400

    updates

--- macros/dbt/incremental_predicates.sql
+++ macros/dbt/incremental_predicates.sql
@@ -1,13 +1,22 @@
 {% macro standard_predicate(
         input_column = 'block_number'
     ) -%}
-    {%- set tmp_table_name = this.identifier ~ '__dbt_tmp' -%}
-    {{ this }}.{{ input_column }} >= (
+    {%- set database_name = target.database -%}
+    {%- set schema_name = generate_schema_name(
+        node = model
+    ) -%}
+    {%- set table_name = generate_alias_name(
+        node = model
+    ) -%}
+    {%- set tmp_table_name = table_name ~ '__dbt_tmp' -%}
+    {%- set full_table_name = database_name ~ '.' ~ schema_name ~ '.' ~ table_name -%}
+    {%- set full_tmp_table_name = database_name ~ '.' ~ schema_name ~ '.' ~ tmp_table_name -%}
+    {{ full_table_name }}.{{ input_column }} >= (
         SELECT
             MIN(
                 {{ input_column }}
             )
         FROM
-            {{ this.database }}.{{ this.schema }}.{{ tmp_table_name }}
+            {{ full_tmp_table_name }}
     )
 {%- endmacro %}
