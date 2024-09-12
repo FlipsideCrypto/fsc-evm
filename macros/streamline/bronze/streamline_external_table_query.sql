21661709252e6a54d3175653c19b5f48d3bce1d6
drethereum <trevor.wenokur@gmail.com>
2024-09-12 13:46:03 -0600

    cast error

--- macros/streamline/bronze/streamline_external_table_query.sql
+++ macros/streamline/bronze/streamline_external_table_query.sql
@@ -28,10 +28,10 @@
         {% if block_number %},
             COALESCE(
                 s.value :"BLOCK_NUMBER" :: INT,
-                s.metadata :request :"data" :id :: INT,
-                PARSE_JSON(
+                (s.metadata :request :"data" :id :: STRING) :: INT,
+                (PARSE_JSON(
                     s.metadata :request :"data"
-                ) :id :: INT
+                ) :id :: STRING) :: INT
             ) AS block_number
         {% endif %}
         FROM
@@ -90,10 +90,10 @@ SELECT
     COALESCE(
         s.value :"BLOCK_NUMBER" :: INT,
         s.value :"block_number" :: INT,
-        s.metadata :request :"data" :id :: INT,
-        PARSE_JSON(
+        (s.metadata :request :"data" :id :: STRING) :: INT,
+        (PARSE_JSON(
             s.metadata :request :"data"
-        ) :id :: INT
+        ) :id :: STRING) :: INT
     ) AS block_number
 {% endif %}
 FROM
