d27ca9c2dbd5bbe2f8e5a61f6ef0daa1fba98123
Austin <austin@flipsidecrypto.com>
2024-09-24 15:49:50 -0400

    simplify and caps

--- macros/streamline/silver/core/streamline_core_complete.sql
+++ macros/streamline/silver/core/streamline_core_complete.sql
@@ -1,65 +1,26 @@
-{% macro streamline_core_complete(
-        model
-    ) %}
+{% macro streamline_core_complete(model) %}
 SELECT
     block_number,
     file_name,
-    {{ dbt_utils.generate_surrogate_key(
-        ['block_number']
-    ) }} AS {% if model == 'blocks' %}
-        complete_blocks_id {% elif model == 'transactions' %}
-        complete_transactions_id {% elif model == 'receipts' %}
-        complete_receipts_id {% elif model == 'traces' %}
-        complete_traces_id {% elif model == 'confirmed_blocks' %}
-        complete_confirmed_blocks_id
-    {% endif %},
+    {{ dbt_utils.generate_surrogate_key(['block_number']) }} AS complete_{{ model }}_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     _inserted_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
+    {% if is_incremental() %}
+        {{ ref('bronze__streamline_' ~ model) }}
+    WHERE
+        _inserted_timestamp >= (
+            SELECT
+                COALESCE(MAX(_inserted_timestamp), '1970-01-01'::TIMESTAMP) AS _inserted_timestamp
+            FROM
+                {{ this }}
+        )
+    {% else %}
+        {{ ref('bronze__streamline_fr_' ~ model) }}
+    {% endif %}
 
-{% if is_incremental() %}
-{% if model == 'blocks' %}
-    {{ ref('bronze__streamline_blocks') }}
+QUALIFY (ROW_NUMBER() OVER (PARTITION BY block_number ORDER BY _inserted_timestamp DESC)) = 1
 
-    {% elif model == 'transactions' %}
-    {{ ref('bronze__streamline_transactions') }}
-
-    {% elif model == 'receipts' %}
-    {{ ref('bronze__streamline_receipts') }}
-
-    {% elif model == 'traces' %}
-    {{ ref('bronze__streamline_traces') }}
-
-    {% elif model == 'confirmed_blocks' %}
-    {{ ref('bronze__streamline_confirmed_blocks') }}
-{% endif %}
-WHERE
-    _inserted_timestamp >= (
-        SELECT
-            COALESCE(MAX(_inserted_timestamp), '1970-01-01' :: TIMESTAMP) AS _inserted_timestamp
-        FROM
-            {{ this }})
-        {% else %}
-            {% if model == 'blocks' %}
-                {{ ref('bronze__streamline_fr_blocks') }}
-
-                {% elif model == 'transactions' %}
-                {{ ref('bronze__streamline_fr_transactions') }}
-
-                {% elif model == 'receipts' %}
-                {{ ref('bronze__streamline_fr_receipts') }}
-
-                {% elif model == 'traces' %}
-                {{ ref('bronze__streamline_fr_traces') }}
-
-                {% elif model == 'confirmed_blocks' %}
-                {{ ref('bronze__streamline_fr_confirmed_blocks') }}
-            {% endif %}
-        {% endif %}
-
-        qualify(ROW_NUMBER() over (PARTITION BY block_number
-        ORDER BY
-            _inserted_timestamp DESC)) = 1
 {% endmacro %}
