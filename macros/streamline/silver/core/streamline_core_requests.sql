40c656d26cd4758cf9b07d5616f2980a2d1624c6
Austin <austin@flipsidecrypto.com>
2024-09-24 12:32:05 -0400

    clean up

--- macros/streamline/silver/core/streamline_core_requests.sql
+++ macros/streamline/silver/core/streamline_core_requests.sql
@@ -50,214 +50,138 @@ SELECT
 ) %}
 
 WITH 
-
 {% if not new_build %}
+    last_3_days AS (
+        SELECT block_number
+        FROM {{ ref("_block_lookback") }}
+    ),
 
-last_3_days AS (
-    SELECT
-        block_number
-    FROM
-        {{ ref("_block_lookback") }}
-),
-{% if model == 'confirmed_blocks' %}
-look_back AS (
-    SELECT
-        block_number
-    FROM
-        {{ ref("_max_block_by_hour") }}
-        qualify ROW_NUMBER() over (
-            ORDER BY
-                block_number DESC
-        ) = 6
-),
+    {% if model == 'confirmed_blocks' %}
+        look_back AS (
+            SELECT block_number
+            FROM {{ ref("_max_block_by_hour") }}
+            QUALIFY ROW_NUMBER() OVER (ORDER BY block_number DESC) = 6
+        ),
+    {% endif %}
 {% endif %}
-{% endif %}
+
 to_do AS (
-    SELECT
-        block_number
-    FROM
-        {{ ref("streamline__blocks") }}
-    WHERE
+    SELECT block_number
+    FROM {{ ref("streamline__blocks") }}
+    WHERE 
         block_number IS NOT NULL
         AND block_number
-        {% if model_type == 'realtime' %}
-        >= 
-        {% elif model_type == 'history' %}
-        <= 
-        {% endif %}
+        {% if model_type == 'realtime' %}>={% elif model_type == 'history' %}<={% endif %}
         {% if not new_build %}
-        (
-                SELECT
-                    block_number
-                FROM
-                    last_3_days
+            (SELECT block_number FROM last_3_days)
+        {% endif %}
+        {% if model == 'confirmed_blocks' and not new_build %}
+            AND block_number <= (SELECT block_number FROM look_back)
+        {% endif %}
+
+    EXCEPT
+
+    SELECT block_number
+    FROM
+        {% if model == 'blocks_transactions' %}
+            {{ ref("streamline__complete_blocks") }} b
+            INNER JOIN {{ ref("streamline__complete_transactions") }} t USING(block_number)
+        {% elif model == 'receipts' %}
+            {{ ref("streamline__complete_receipts") }}
+        {% elif model == 'traces' %}
+            {{ ref("streamline__complete_traces") }}
+        {% elif model == 'confirmed_blocks' %}
+            {{ ref("streamline__complete_confirmed_blocks") }}
+        {% endif %}
+    WHERE 1=1
+        {% if not new_build %}
+            AND block_number
+            {% if model_type == 'realtime' %}>={% elif model_type == 'history' %}<={% endif %}
+            (SELECT block_number FROM last_3_days)
+            {% if model == 'confirmed_blocks' %}
+                AND block_number IS NOT NULL
+                AND block_number <= (SELECT block_number FROM look_back)
+                AND _inserted_timestamp >= DATEADD('day', -4, SYSDATE())
+                AND block_number >= (SELECT block_number FROM last_3_days)
+            {% endif %}
+        {% endif %}
+)
+
+{% if model != 'confirmed_blocks' %}
+    ,ready_blocks AS (
+        SELECT block_number
+        FROM to_do
+
+        {% if not new_build %}
+            UNION
+            SELECT block_number
+            FROM (
+                SELECT block_number
+                FROM {{ ref("_unconfirmed_blocks") }}
+
+                UNION
+
+                {% if model == 'blocks_transactions' %}
+                    SELECT block_number
+                    FROM {{ ref("_missing_txs") }}
+                {% elif model == 'receipts' %}
+                    SELECT block_number
+                    FROM {{ ref("_missing_txs") }}
+                    UNION
+                    SELECT block_number
+                    FROM {{ ref("_missing_receipts") }}
+                {% elif model == 'traces' %}
+                    SELECT block_number
+                    FROM {{ ref("_missing_traces") }}
+                {% endif %}
             )
         {% endif %}
-        {% if model == 'confirmed_blocks' %}
-        {% if not new_build %}
-        AND block_number <= (
-            SELECT
-                block_number
-            FROM
-                look_back
-        )
-        {% endif %}
-    {% endif %}
-    EXCEPT
-    SELECT
-        block_number
-    FROM
-    {% if model == 'blocks_transactions' %}
-        {{ ref("streamline__complete_blocks") }}
-        b
-        INNER JOIN {{ ref("streamline__complete_transactions") }}
-        t USING(block_number) -- inner join to ensure that only blocks with both block data and transaction data are excluded
-    {% elif model == 'receipts' %}
-        {{ ref("streamline__complete_receipts") }}
-    {% elif model == 'traces' %}
-        {{ ref("streamline__complete_traces") }}
-    {% elif model == 'confirmed_blocks' %}
-        {{ ref("streamline__complete_confirmed_blocks") }}
-    {% endif %}
-    WHERE
-        1=1
-        {% if not new_build %}
-        and block_number
-        {% if model_type == 'realtime' %}
-        >= 
-        {% elif model_type == 'history' %}
-        <= 
-        {% endif %}
-        (
-            SELECT
-                block_number
-            FROM
-                last_3_days
-        )
-    {% endif %}
-    {% if model == 'confirmed_blocks' %}
-        AND block_number IS NOT NULL
-        {% if not new_build %}
-        AND block_number <= (
-            SELECT
-                block_number
-            FROM
-                look_back
-        )
-        {% endif %}
-        AND _inserted_timestamp >= DATEADD(
-            'day',
-            -4,
-            SYSDATE()
-        )
-        AND block_number >= (
-            SELECT
-                block_number
-            FROM
-                last_3_days
-        )
-    {% endif %}
-    {% endif %}
-)
-{% if model != 'confirmed_blocks' %}
-,ready_blocks AS (
-    SELECT
-        block_number
-    FROM
-        to_do
-    {% if not new_build %}
-    UNION
-    SELECT
-        block_number
-    FROM (
-        SELECT
-            block_number
-        FROM
-            {{ ref("_unconfirmed_blocks") }}
-        UNION
-    {% if model == 'blocks_transactions' %}
-    SELECT
-        block_number
-    FROM
-        {{ ref("_missing_txs") }}
-    {% elif model == 'receipts' %}
-    SELECT
-        block_number
-    FROM
-        {{ ref("_missing_txs") }}
-    UNION
-    SELECT
-        block_number
-    FROM
-        {{ ref("_missing_receipts") }}
-    {% elif model == 'traces' %}
-    SELECT
-        block_number
-    FROM
-        {{ ref("_missing_traces") }}
-    {% endif %}
     )
-    {% endif %}
-)
 {% endif %}
+
 SELECT
     block_number,
-    ROUND(
-        block_number,
-        -3
-    ) AS partition_key,
+    ROUND(block_number, -3) AS partition_key,
     live.udf_api(
         'POST',
         '{{ api_url }}',
         OBJECT_CONSTRUCT(
-            'Content-Type',
-            'application/json'
-        {% if quantum_state == 'streamline' %}
-            ,'fsc-quantum-state',
-            'streamline'
+            'Content-Type', 'application/json'
+            {% if quantum_state == 'streamline' %}
+                ,'fsc-quantum-state', 'streamline'
+            {% elif quantum_state == 'livequery' %}
+                ,'fsc-quantum-state', 'livequery'
+            {% endif %}
         ),
-        {% elif quantum_state == 'livequery' %}
-            ,'fsc-quantum-state',
-            'livequery'
-        ),
-        {% else %}
-        ),
-        {% endif %}
         OBJECT_CONSTRUCT(
-            'id',
-            block_number,
-            'jsonrpc',
-            '2.0',
+            'id', block_number,
+            'jsonrpc', '2.0',
             'method',
-        {% if model == 'blocks_transactions' %}
-            'eth_getBlockByNumber',
-            'params',
-            ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number), TRUE)),
-            --set to TRUE for full txn data
-        {% elif model == 'receipts' %}
-            'eth_getBlockReceipts',
-            'params',
-            ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number))),
-        {% elif model == 'traces' %}
-            'debug_traceBlockByNumber',
-            'params',
-            ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number), OBJECT_CONSTRUCT('tracer', 'callTracer', 'timeout', '30s'))),
-        {% elif model == 'confirmed_blocks' %}
-            'eth_getBlockByNumber',
-            'params',
-            ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number), FALSE)),
-        {% endif %}
+            {% if model == 'blocks_transactions' %}
+                'eth_getBlockByNumber',
+                'params', ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number), TRUE)
+            {% elif model == 'receipts' %}
+                'eth_getBlockReceipts',
+                'params', ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number))
+            {% elif model == 'traces' %}
+                'debug_traceBlockByNumber',
+                'params', ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number), OBJECT_CONSTRUCT('tracer', 'callTracer', 'timeout', '30s'))
+            {% elif model == 'confirmed_blocks' %}
+                'eth_getBlockByNumber',
+                'params', ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number), FALSE)
+            {% endif %}
+        ),
         '{{ vault_secret_path }}'
     ) AS request
-        FROM
-            {% if model != 'confirmed_blocks' %}
-                ready_blocks
-            {% else %}
-                to_do
-            {% endif %}
-        {{ order_by_clause }}
-        {% if query_limit %}
-        LIMIT
-            {{ query_limit }} 
-        {% endif %}
+FROM
+    {% if model != 'confirmed_blocks' %}
+        ready_blocks
+    {% else %}
+        to_do
+    {% endif %}
+{{ order_by_clause }}
+{% if query_limit %}
+    LIMIT {{ query_limit }} 
+{% endif %}
+
 {% endmacro %}
