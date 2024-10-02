3845c019fc86db3d855e9564a9da52dc2535f340
Austin <austin@flipsidecrypto.com>
2024-10-02 16:42:59 -0400

    chainhead

--- macros/streamline/silver/core/v2/chainhead.sql
+++ macros/streamline/silver/core/v2/chainhead.sql
@@ -5,16 +5,25 @@
 
 {# Log configuration details if in execution mode #}
 {%- if execute -%}
-    {{ log("", info=True) }}
-    {{ log("=== Model Configuration ===", info=True) }}
-    {{ log("Model Quantum State: " ~ model_quantum_state, info=True) }}
-    {{ log("Materialization: " ~ config.get('materialized'), info=True) }}
+
+    {{ log("=== Current Variable Settings ===", info=True) }}
+    {{ log("CHAINHEAD_QUANTUM_STATE: " ~ model_quantum_state, info=True) }}
     {{ log("", info=True) }}
 
     {{ log("=== API Details ===", info=True) }}
-    {{ log("API URL: " ~ var('API_URL'), info=True) }}
-    {{ log("Vault Secret Path: " ~ var('VAULT_SECRET_PATH'), info=True) }}
+    {{ log("API_URL: " ~ var('API_URL'), info=True) }}
+    {{ log("VAULT_SECRET_PATH: " ~ var('VAULT_SECRET_PATH'), info=True) }}
     {{ log("", info=True) }}
+
+    {% set config_log = '\n' %}
+    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
+    {% set config_log = config_log ~ '\n{{ config (\n' %}
+    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
+    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') | tojson ~ '\n' %}
+    {% set config_log = config_log ~ ') }}\n' %}
+    {{ log(config_log, info=True) }}
+    {{ log("", info=True) }}
+
 {%- endif -%}
 
 {{ config (
