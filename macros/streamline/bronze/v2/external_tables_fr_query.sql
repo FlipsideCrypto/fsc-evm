a7cf3231b810c8bfea8bb57d8ce9aa33fd0c8388
Austin <austin@flipsidecrypto.com>
2024-10-01 22:14:27 -0400

    fr v2

--- macros/streamline/bronze/v2/external_tables_fr_query.sql
+++ macros/streamline/bronze/v2/external_tables_fr_query.sql
@@ -1,4 +1,4 @@
-{% macro streamline_external_table_query_v2() %}
+{% macro streamline_external_table_fr_query_v2() %}
 
 {# Extract model information from the identifier #}
 {%- set identifier_parts = this.identifier.split('__') -%}
@@ -15,8 +15,10 @@
 {# Set parameters using project variables #}
 {% set partition_function = var((model ~ '_partition_function').upper(), 
     'CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER)') %}
+{% set partition_join_key = var((model ~ '_partition_join_key').upper(), 'partition_key') %}
 {% set balances = var((model ~ '_balances').upper(), false) %}
 {% set block_number = var((model ~ '_block_number').upper(), true) %}
+
 
 {# Log configuration details if in execution mode #}
 {% if execute %}
@@ -25,6 +27,7 @@
     {{ log("Model: " ~ model, info=True) }}
     {{ log("Model Type: " ~ model_type, info=True) }}
     {{ log("Partition Function: " ~ partition_function, info=True) }}
+    {{ log("Partition Join Key: " ~ partition_join_key, info=True) }}
     {{ log("Balances: " ~ balances, info=True) }}
     {{ log("Block Number: " ~ block_number, info=True) }}
     {{ log("Materialization: " ~ config.get('materialized'), info=True) }}
@@ -33,62 +36,59 @@
     {{ log("=== Source Details ===", info=True) }}
     {{ log("Source: " ~ source('bronze_streamline', model), info=True) }}
     {{ log("", info=True) }}
+    
 {% endif %}
-
-{{ config (
-    materialized = 'view'
-) }}
-
     WITH meta AS (
         SELECT
-            job_created_time AS _inserted_timestamp,
+            registered_on AS _inserted_timestamp,
             file_name,
             {{ partition_function }} AS partition_key
         FROM
             TABLE(
-                information_schema.external_table_file_registration_history(
-                    start_time => DATEADD('day', -3, CURRENT_TIMESTAMP()),
-                    table_name => '{{ source( "bronze_streamline", model) }}')
-                ) A
-            )
-        SELECT
-            s.*,
-            b.file_name,
-            b._inserted_timestamp
+                information_schema.external_table_files(
+                    table_name => '{{ source( "bronze_streamline", model) }}'
+                )
+            ) A
+    )
+SELECT
+    s.*,
+    b.file_name,
+    b._inserted_timestamp
 
-        {% if balances %},
-            r.block_timestamp :: TIMESTAMP AS block_timestamp
-        {% endif %}
+    {% if balances %},
+    r.block_timestamp :: TIMESTAMP AS block_timestamp
+{% endif %}
 
-        {% if block_number %},
-            COALESCE(
-                s.value :"BLOCK_NUMBER" :: STRING,
-                s.metadata :request :"data" :id :: STRING,
-                PARSE_JSON(
-                    s.metadata :request :"data"
-                ) :id :: STRING
-            ) :: INT AS block_number
-        {% endif %}
-        FROM
-            {{ source(
-                "bronze_streamline",
-                model
-            ) }}
-            s
-            JOIN meta b
-            ON b.file_name = metadata$filename
-            AND b.partition_key = s.partition_key
+{% if block_number %},
+    COALESCE(
+        s.value :"BLOCK_NUMBER" :: STRING,
+        s.value :"block_number" :: STRING,
+        s.metadata :request :"data" :id :: STRING,
+        PARSE_JSON(
+            s.metadata :request :"data"
+        ) :id :: STRING
+    ) :: INT AS block_number
+{% endif %}
+FROM
+    {{ source(
+        "bronze_streamline",
+        model
+    ) }}
+    s
+    JOIN meta b
+    ON b.file_name = metadata$filename
+    AND b.partition_key = s.{{ partition_join_key }}
 
-            {% if balances %}
-            JOIN {{ ref('_block_ranges') }}
-            r
-            ON r.block_number = COALESCE(
-                s.value :"BLOCK_NUMBER" :: INT,
-                s.value :"block_number" :: INT
-            )
-        {% endif %}
-        WHERE
-            b.partition_key = s.partition_key
-            AND DATA :error IS NULL
-            AND DATA IS NOT NULL
+    {% if balances %}
+        JOIN {{ ref('_block_ranges') }}
+        r
+        ON r.block_number = COALESCE(
+            s.value :"BLOCK_NUMBER" :: INT,
+            s.value :"block_number" :: INT
+        )
+    {% endif %}
+WHERE
+    b.partition_key = s.{{ partition_join_key }}
+    AND DATA :error IS NULL
+    AND DATA IS NOT NULL
 {% endmacro %}
