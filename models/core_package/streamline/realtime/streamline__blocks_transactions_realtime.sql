383b518d2b7af23e900cf8bfcc79a2b5488a7de1
Austin <austin@flipsidecrypto.com>
2024-10-04 23:42:38 -0400

    macro logs

--- models/core_package/streamline/realtime/streamline__blocks_transactions_realtime.sql
+++ models/core_package/streamline/realtime/streamline__blocks_transactions_realtime.sql
@@ -15,50 +15,17 @@
 
 {%- set default_vars = set_default_variables(model_name, model_type) -%}
 
-{# Log configuration details if in dev or during execution #}
-{%- if execute and not target.name.startswith('prod') -%}
-
-    {{ log("=== API Details ===", info=True) }}
-
-    {{ log("NODE_URL: " ~ node_url, info=True) }}
-    {{ log("NODE_SECRET_PATH: " ~ var('NODE_SECRET_PATH'), info=True) }}
-    {{ log("", info=True) }}
-
-    {{ log("=== Current Variable Settings ===", info=True) }}
-
-    {{ log((model_name ~ '_' ~ model_type ~ '_model_quantum_state').upper() ~ ': ' ~ model_quantum_state, info=True) }}
-    {{ log((model_name ~ '_' ~ model_type ~ '_sql_limit').upper() ~ ': ' ~ sql_limit, info=True) }}
-    {{ log((model_name ~ '_' ~ model_type ~ '_testing_limit').upper() ~ ': ' ~ testing_limit, info=True) }}
-    {{ log((model_name ~ '_' ~ model_type ~ '_order_by_clause').upper() ~ ': ' ~ order_by_clause, info=True) }}
-    {{ log((model_name ~ '_' ~ model_type ~ '_new_build').upper() ~ ': ' ~ new_build, info=True) }}
-    {{ log("", info=True) }}
-
-    {{ log("=== RPC Details ===", info=True) }}
-
-    {{ log(model_name ~ ": {", info=True) }}
-    {{ log("    method: '" ~ 'eth_getBlockByNumber' ~ "',", info=True) }}
-    {{ log("    params: '" ~ 'ARRAY_CONSTRUCT(utils.udf_int_to_hex(block_number), TRUE)' ~ "'", info=True) }}
-    {{ log("}", info=True) }}
-    {{ log("", info=True) }}
-
-    {% set params_str = streamline_params | tojson %}
-    {% set params_formatted = params_str | replace('{', '{\n            ') | replace('}', '\n        }') | replace(', ', ',\n            ') %}
-
-    {% set config_log = '\n' %}
-    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
-    {% set config_log = config_log ~ '\n{{ config (\n' %}
-    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
-    {% set config_log = config_log ~ '    post_hook = fsc_utils.if_data_call_function_v2(\n' %}
-    {% set config_log = config_log ~ '        func = "streamline.udf_bulk_rest_api_v2",\n' %}
-    {% set config_log = config_log ~ '        target = "' ~ this.schema ~ '.' ~ this.identifier ~ '",\n' %}
-    {% set config_log = config_log ~ '        params = ' ~ params_formatted ~ '\n' %}
-    {% set config_log = config_log ~ '    ),\n' %}
-    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') | tojson ~ '\n' %}
-    {% set config_log = config_log ~ ') }}\n' %}
-    {{ log(config_log, info=True) }}
-    {{ log("", info=True) }}
-
-{%- endif -%}
+{{ log_streamline_details(
+    model_name=model_name,
+    model_type=model_type,
+    node_url=default_vars['node_url'],
+    model_quantum_state=default_vars['model_quantum_state'],
+    sql_limit=streamline_params['sql_limit'],
+    testing_limit=default_vars['testing_limit'],
+    order_by_clause=default_vars['order_by_clause'],
+    new_build=default_vars['new_build'],
+    streamline_params=streamline_params
+) }}
 
 {# Set up dbt configuration #}
 {{ config (
