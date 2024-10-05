e297af0edc41d8972e97129b290f1118a41f3055
Austin <austin@flipsidecrypto.com>
2024-10-05 00:05:11 -0400

    bronze

---
+++ macros/core_package/logging/bronze.sql
@@ -0,0 +1,29 @@
+{% macro log_bronze_details(source_name, model_type, partition_function, partition_join_key, block_number, uses_receipts_by_hash) %}
+
+{%- if execute and not target.name.startswith('prod') -%}
+
+    {{ log("=== Current Variable Settings ===", info=True) }}
+    {{ log(source_name ~ model_type ~ '_PARTITION_FUNCTION: ' ~ partition_function, info=True) }}
+    {{ log(source_name ~ model_type ~ '_PARTITION_JOIN_KEY: ' ~ partition_join_key, info=True) }}
+    {{ log(source_name ~ model_type ~ '_BLOCK_NUMBER: ' ~ block_number, info=True) }}
+    {% if uses_receipts_by_hash %}
+        {{ log("USES_RECEIPTS_BY_HASH: " ~ uses_receipts_by_hash, info=True) }}
+    {% endif %}
+
+    {{ log("", info=True) }}
+    {{ log("=== Source Details ===", info=True) }}
+    {{ log("Source: " ~ source('bronze_streamline', source_name.lower()), info=True) }}
+    {{ log("", info=True) }}
+
+    {% set config_log = '\n' %}
+    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
+    {% set config_log = config_log ~ '\n{{ config (\n' %}
+    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
+    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') | tojson ~ '\n' %}
+    {% set config_log = config_log ~ ') }}\n' %}
+    {{ log(config_log, info=True) }}
+    {{ log("", info=True) }}
+
+{%- endif -%}
+
+{% endmacro %}
--- models/core_package/bronze/bronze__blocks.sql
+++ models/core_package/bronze/bronze__blocks.sql
@@ -1,41 +1,16 @@
 {% set source_name = var('BLOCKS_SOURCE_NAME', 'BLOCKS') %}
-
 {% set model_type = '' %}
 
-{# Default dynamic variables begin #}
+{%- set default_vars = set_default_variables_bronze(source_name, model_type) -%}
 
-{% set partition_function = var(source_name ~ model_type ~ '_PARTITION_FUNCTION', 
- "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER)") 
-%}
-
-{% set partition_join_key = var(source_name ~ model_type ~ '_PARTITION_JOIN_KEY', 'partition_key') %}
-{% set block_number = var(source_name ~ model_type ~ '_BLOCK_NUMBER', True) %}
-
-{# Default variables end #}
-
-{# Log configuration details if in dev or during execution #}
-{%- if execute and not target.name.startswith('prod') -%}
-
-    {{ log("=== Current Variable Settings ===", info=True) }}
-    {{ log(source_name ~ model_type ~ '_PARTITION_FUNCTION: ' ~ partition_function, info=True) }}
-    {{ log(source_name ~ model_type ~ '_PARTITION_JOIN_KEY: ' ~ partition_join_key, info=True) }}
-    {{ log(source_name ~ model_type ~ '_BLOCK_NUMBER: ' ~ block_number, info=True) }}
-
-    {{ log("", info=True) }}
-    {{ log("=== Source Details ===", info=True) }}
-    {{ log("Source: " ~ source('bronze_streamline', source_name.lower()), info=True) }}
-    {{ log("", info=True) }}
-
-    {% set config_log = '\n' %}
-    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
-    {% set config_log = config_log ~ '\n{{ config (\n' %}
-    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
-    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') | tojson ~ '\n' %}
-    {% set config_log = config_log ~ ') }}\n' %}
-    {{ log(config_log, info=True) }}
-    {{ log("", info=True) }}
-
-{% endif %}
+{{ log_bronze_details(
+    source_name = source_name,
+    model_type = model_type,
+    partition_function = default_vars['partition_function'],
+    partition_join_key = default_vars['partition_join_key'],
+    block_number = default_vars['block_number'],
+    uses_receipts_by_hash = default_vars['uses_receipts_by_hash']
+) }}
 
 {{ config (
     materialized = 'view',
