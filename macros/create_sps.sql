35166d6e14438691e6de9b1d8c76804ebfe34193
Austin <austin@flipsidecrypto.com>
2024-10-07 14:59:15 -0400

    debug

--- macros/create_sps.sql
+++ macros/create_sps.sql
@@ -1,10 +1,30 @@
 {% macro create_sps() %}
+    {{ log("Debug: Entering create_sps macro", info=True) }}
     {% if var("UPDATE_UDFS_AND_SPS", false) %}
+        {{ log("Debug: UPDATE_UDFS_AND_SPS is true", info=True) }}
         {% set prod_db_name = var('PROD_DB_NAME') | upper %}
+        {{ log("Debug: prod_db_name is " ~ prod_db_name, info=True) }}
         {% if target.database | upper == prod_db_name and target.name == 'prod' %}
+            {{ log("Debug: Target database and name match production criteria", info=True) }}
             {% set schema_name = var("SPS_SCHEMA_NAME", '_internal') %}
+            {{ log("Debug: schema_name is " ~ schema_name, info=True) }}
             {% do run_query("CREATE SCHEMA IF NOT EXISTS " ~ schema_name) %}
-            {{ sp_create_prod_clone(schema_name) }}
+            {{ log("Debug: Attempting to call sp_create_prod_clone", info=True) }}
+            {% if execute %}
+                {% if context.get('sp_create_prod_clone') is not none %}
+                    {{ log("Debug: sp_create_prod_clone is defined", info=True) }}
+                    {{ sp_create_prod_clone(schema_name) }}
+                {% else %}
+                    {{ log("Warning: sp_create_prod_clone is not defined. Skipping stored procedure creation.", info=True) }}
+                {% endif %}
+            {% else %}
+                {{ log("Debug: In compilation phase, skipping sp_create_prod_clone call", info=True) }}
+            {% endif %}
+        {% else %}
+            {{ log("Debug: Target database or name do not match production criteria", info=True) }}
         {% endif %}
+    {% else %}
+        {{ log("Debug: UPDATE_UDFS_AND_SPS is false", info=True) }}
     {% endif %}
+    {{ log("Debug: Exiting create_sps macro", info=True) }}
 {% endmacro %}
