4e3692d8fd0cf99ce531dc521aa04fb58cbc775c
Austin <austin@flipsidecrypto.com>
2024-10-07 14:56:50 -0400

    test

--- macros/create_sps.sql
+++ macros/create_sps.sql
@@ -3,8 +3,8 @@
         {% set prod_db_name = var('PROD_DB_NAME') | upper %}
         {% if target.database | upper == prod_db_name and target.name == 'prod' %}
             {% set schema_name = var("SPS_SCHEMA_NAME", '_internal') %}
-            CREATE SCHEMA IF NOT EXISTS {{ schema_name }};
-            {{ sp_create_prod_clone(schema_name) }};
+            {% do run_query("CREATE SCHEMA IF NOT EXISTS " ~ schema_name) %}
+            {{ sp_create_prod_clone(schema_name) }}
         {% endif %}
     {% endif %}
 {% endmacro %}
