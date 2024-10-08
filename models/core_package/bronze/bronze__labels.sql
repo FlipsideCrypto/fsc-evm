fe2954f447fa46afa0dc1273e8ea42cd482a944b
Austin <austin@flipsidecrypto.com>
2024-10-08 16:34:19 -0400

    bronze labels

---
+++ models/core_package/bronze/bronze__labels.sql
@@ -0,0 +1,48 @@
+{%- set blockchains = var('LABELS_BLOCKCHAINS', var('PROD_DB_NAME').lower() ) -%}
+
+{%- if flags.WHICH == 'compile' and execute -%}
+
+    {{ log("=== Current Variable Settings ===", info=True) }}
+
+    {{ log("LABELS_BLOCKCHAINS: " ~ blockchains, info=True) }}
+    {{ log("", info=True) }}
+
+    {% set config_log = '\n' %}
+    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
+    {% set config_log = config_log ~ '\n{{ config (\n' %}
+    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '"\n' %}
+    {% set config_log = config_log ~ ') }}\n' %}
+    {{ log(config_log, info=True) }}
+    {{ log("", info=True) }}
+
+{%- endif -%}
+
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT
+    system_created_at,
+    insert_date,
+    blockchain,
+    address,
+    creator,
+    label_type,
+    label_subtype,
+    address_name,
+    project_name,
+    _is_deleted,
+    modified_timestamp,
+    labels_combined_id
+FROM
+    {{ source(
+        'crosschain_silver',
+        'labels_combined'
+    ) }}
+WHERE
+    blockchain IN ({% if blockchains is string %}
+        '{{ blockchains }}'
+    {% else %}
+        {{ blockchains | replace('[', '') | replace(']', '') }}
+    {% endif %})
+    AND address LIKE '0x%'
