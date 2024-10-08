5406367aff6e9103461dd4c406f732612241f31b
drethereum <trevor.wenokur@gmail.com>
2024-10-08 16:34:48 -0600

    price models

---
+++ models/core_package/bronze/prices/bronze_complete_token_asset_metadata.sql
@@ -0,0 +1,66 @@
+{# Set variables #}
+{%- set token_addresses = var('PRICES_TOKEN_ADDRESSES') -%}
+{%- set blockchains = var('PRICES_BLOCKCHAINS', var('PROD_DB_NAME').lower() ) -%}
+
+{# Log configuration details #}
+{%- if flags.WHICH == 'compile' and execute -%}
+
+    {{ log("=== Current Variable Settings ===", info=True) }}
+
+    {{ log("PRICES_TOKEN_ADDRESSES: " ~ token_addresses, info=True) }}
+    {{ log("PRICES_BLOCKCHAINS: " ~ blockchains, info=True) }}
+    {{ log("", info=True) }}
+
+    {% set config_log = '\n' %}
+    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
+    {% set config_log = config_log ~ '\n{{ config (\n' %}
+    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
+    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') ~ '\n' %}
+    {% set config_log = config_log ~ ') }}\n' %}
+    {{ log(config_log, info=True) }}
+    {{ log("", info=True) }}
+    
+{%- endif -%}
+
+{# Set up dbt configuration #}
+{{ config (
+    materialized = 'view',
+    tags = ['core','bronze','prices']
+) }}
+
+{# Main query starts here #}
+SELECT
+    token_address,
+    asset_id,
+    symbol,
+    NAME,
+    decimals,
+    blockchain,
+    blockchain_name,
+    blockchain_id,
+    is_deprecated,
+    provider,
+    source,
+    _inserted_timestamp,
+    inserted_timestamp,
+    modified_timestamp,
+    complete_token_asset_metadata_id,
+    _invocation_id
+FROM
+    {{ source(
+        'crosschain_silver',
+        'complete_token_asset_metadata'
+    ) }}
+WHERE
+    blockchain IN ({% if blockchains is string %}
+        '{{ blockchains }}'
+    {% else %}
+        {{ blockchains | replace('[', '') | replace(']', '') }}
+    {% endif %})
+    {% if token_addresses %}
+        AND token_address IN ({% if token_addresses is string %}
+            '{{ token_addresses }}'
+        {% else %}
+            {{ token_addresses | replace('[', '') | replace(']', '') }}
+        {% endif %})
+    {% endif %}
---
+++ models/core_package/silver/prices/silver_complete_provider_prices.sql
@@ -0,0 +1,65 @@
+{# Log configuration details #}
+{%- if flags.WHICH == 'compile' and execute -%}
+
+    {% set config_log = '\n' %}
+    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
+    {% set config_log = config_log ~ '\n{{ config (\n' %}
+    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
+    {% set config_log = config_log ~ '    incremental_strategy = "' ~ config.get('incremental_strategy') ~ '",\n' %}
+    {% set config_log = config_log ~ '    unique_key = "' ~ config.get('unique_key') ~ '",\n' %}
+    {% set config_log = config_log ~ '    cluster_by = ' ~ config.get('cluster_by') ~ ',\n' %}
+    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') ~ '\n' %}
+    {% set config_log = config_log ~ ') }}\n' %}
+    {{ log(config_log, info=True) }}
+    {{ log("", info=True) }}
+
+{%- endif -%}
+
+{# Set up dbt configuration #}
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'complete_provider_prices_id',
+    cluster_by = ['recorded_hour::DATE','provider'],
+    tags = ['core','silver','prices']
+) }}
+
+{# Main query starts here #}
+SELECT
+    p.asset_id,
+    recorded_hour,
+    OPEN,
+    high,
+    low,
+    CLOSE,
+    p.provider,
+    p.source,
+    p._inserted_timestamp,
+    p.inserted_timestamp,
+    p.modified_timestamp,
+    p.complete_provider_prices_id,
+    p._invocation_id
+FROM
+    {{ ref(
+        'bronze__complete_provider_prices'
+    ) }}
+    p
+    INNER JOIN {{ ref('bronze__complete_provider_asset_metadata') }}
+    m
+    ON p.asset_id = m.asset_id
+
+{% if is_incremental() %}
+WHERE
+    p.modified_timestamp >= (
+        SELECT
+            MAX(
+                modified_timestamp
+            )
+        FROM
+            {{ this }}
+    )
+{% endif %}
+
+qualify(ROW_NUMBER() over (PARTITION BY p.asset_id, recorded_hour, p.provider
+ORDER BY
+    p.modified_timestamp DESC)) = 1
