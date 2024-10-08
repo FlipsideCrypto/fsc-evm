126ec7ce1de79fe990a6e74031225ab5148192f1
drethereum <trevor.wenokur@gmail.com>
2024-10-08 16:34:48 -0600

    price models

---
+++ models/core_package/bronze/prices/bronze_complete_native_asset_metadata.sql
@@ -0,0 +1,61 @@
+{# Set variables #}
+{%- set symbols = var('PRICES_SYMBOLS') -%}
+{%- set blockchains = var('PRICES_BLOCKCHAINS', var('PROD_DB_NAME').lower() ) -%}
+
+{# Log configuration details #}
+{%- if flags.WHICH == 'compile' and execute -%}
+
+    {{ log("=== Current Variable Settings ===", info=True) }}
+
+    {{ log("PRICES_SYMBOLS: " ~ symbols, info=True) }}
+    {{ log("PRICES_BLOCKCHAINS: " ~ blockchains, info=True) }}
+    {{ log("", info=True) }}
+
+    {% set config_log = '\n' %}
+    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
+    {% set config_log = config_log ~ '\n{{ config (\n' %}
+    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
+    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') ~ '\n' %}
+    {% set config_log = config_log ~ ') }}\n' %}
+    {{ log(config_log, info=True) }}
+    {{ log("", info=True) }}
+
+{%- endif -%}
+
+{# Set up dbt configuration #}
+{{ config (
+    materialized = 'view',
+    tags = ['core','bronze','prices']
+) }}
+
+{# Main query starts here #}
+SELECT
+    asset_id,
+    symbol,
+    NAME,
+    decimals,
+    blockchain,
+    is_deprecated,
+    provider,
+    source,
+    _inserted_timestamp,
+    inserted_timestamp,
+    modified_timestamp,
+    complete_native_asset_metadata_id,
+    _invocation_id
+FROM
+    {{ source(
+        'crosschain_silver',
+        'complete_native_asset_metadata'
+    ) }}
+WHERE
+    blockchain IN ({% if blockchains is string %}
+        '{{ blockchains }}'
+    {% else %}
+        {{ blockchains | replace('[', '') | replace(']', '') }}
+    {% endif %})
+    AND symbol IN ({% if symbols is string %}
+        '{{ symbols }}'
+    {% else %}
+        {{ symbols | replace('[', '') | replace(']', '') }}
+    {% endif %})
---
+++ models/core_package/silver/prices/silver_complete_token_prices.sql
@@ -0,0 +1,66 @@
+{# Log configuration details #}
+{%- if flags.WHICH == 'compile' and execute -%}
+
+    {% set config_log = '\n' %}
+    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
+    {% set config_log = config_log ~ '\n{{ config (\n' %}
+    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
+    {% set config_log = config_log ~ '    incremental_strategy = "' ~ config.get('incremental_strategy') ~ '",\n' %}
+    {% set config_log = config_log ~ '    unique_key = "' ~ config.get('unique_key') ~ '",\n' %}    
+    {% set config_log = config_log ~ '    cluster_by = ' ~ config.get('cluster_by') ~ ',\n' %}
+    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') ~ '\n' %}
+    {% set config_log = config_log ~ ') }}\n' %}
+    {{ log(config_log, info=True) }}
+    {{ log("", info=True) }}
+
+{%- endif -%}
+
+{# Set up dbt configuration #}
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'complete_token_prices_id',
+    cluster_by = ['hour::DATE'],
+    tags = ['core','silver','prices']
+) }}
+
+{# Main query starts here #}
+SELECT
+    HOUR,
+    LOWER(
+        p.token_address
+    ) AS token_address,
+    asset_id,
+    symbol,
+    NAME,
+    decimals,
+    price,
+    blockchain,
+    blockchain_name,
+    blockchain_id,
+    is_imputed,
+    is_deprecated,
+    provider,
+    source,
+    _inserted_timestamp,
+    inserted_timestamp,
+    modified_timestamp,
+    complete_token_prices_id,
+    _invocation_id
+FROM
+    {{ ref(
+        'bronze__complete_token_prices'
+    ) }}
+    p
+
+{% if is_incremental() %}
+WHERE
+    modified_timestamp >= (
+        SELECT
+            MAX(
+                modified_timestamp
+            )
+        FROM
+            {{ this }}
+    )
+{% endif %}
