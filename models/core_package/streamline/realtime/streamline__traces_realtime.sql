3aed7c310cad03067c08114bf5679bbcb0c912d8
Austin <austin@flipsidecrypto.com>
2024-10-10 11:09:45 -0400

    realtime

--- models/core_package/streamline/realtime/streamline__traces_realtime.sql
+++ models/core_package/streamline/realtime/streamline__traces_realtime.sql
@@ -1,3 +1,4 @@
+{%- if var('GLOBAL_USES_V2_FSC_EVM', False) -%}
 {% set model_name = 'TRACES' %}
 {% set model_type = 'REALTIME' %}
 {%- set min_block = var('GLOBAL_START_UP_BLOCK', none) -%}
@@ -111,3 +112,5 @@ FROM
 {{ default_vars['order_by_clause'] }}
 
 LIMIT {{ streamline_params['sql_limit'] }}
+
+{%- endif -%}
