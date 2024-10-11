36a90a47b960ea4ebe80c7103e05cf837e4be503
drethereum <trevor.wenokur@gmail.com>
2024-10-11 12:42:45 -0600

    bronze core fr

---
+++ models/core_package/bronze/core/bronze__receipts_fr.sql
@@ -0,0 +1,27 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT
+    partition_key,
+    block_number,
+    array_index,
+    VALUE,
+    DATA,
+    metadata,
+    file_name,
+    _inserted_timestamp
+FROM
+    {{ ref('bronze__receipts_fr_v2') }}
+UNION ALL
+SELECT
+    _partition_by_block_id AS partition_key,
+    block_number,
+    VALUE :"array_index" :: INT AS array_index,
+    VALUE,
+    DATA,
+    metadata,
+    file_name,
+    _inserted_timestamp
+FROM
+   {{ ref('bronze__receipts_fr_v1') }}
---
+++ models/core_package/bronze/core/bronze__transactions_fr_v1.sql
@@ -0,0 +1,30 @@
+{% set source_name = 'TRANSACTIONS' %}
+{% set source_version = '' %}
+{% set model_type = 'FR' %}
+
+{%- set default_vars = set_default_variables_bronze(source_name, model_type) -%}
+
+{{ log_bronze_details(
+    source_name = source_name,
+    source_version = source_version,
+    model_type = model_type,
+    partition_function = default_vars['partition_function'],
+    partition_join_key = default_vars['partition_join_key'],
+    block_number = default_vars['block_number'],
+    uses_receipts_by_hash = default_vars['uses_receipts_by_hash']
+) }}
+
+{{ config (
+    materialized = 'view',
+    tags = ['streamline_core_complete', 'bronze_external']
+) }}
+
+{{ streamline_external_table_query_fr(
+    source_name = source_name.lower(),
+    source_version = source_version.lower(),
+    partition_function = default_vars['partition_function'],
+    partition_join_key = '_partition_by_block_id',
+    balances = default_vars['balances'],
+    block_number = false,
+    uses_receipts_by_hash = default_vars['uses_receipts_by_hash']
+) }}
