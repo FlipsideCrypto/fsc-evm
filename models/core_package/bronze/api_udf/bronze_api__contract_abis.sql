454add0a23e52a0c706d38ffeecb0697b30c3abe
sam <sam@flipsidecrypto.com>
2024-10-15 22:57:46 +0800

    add new

--- models/core_package/bronze/api_udf/bronze_api__contract_abis.sql
+++ models/core_package/bronze/api_udf/bronze_api__contract_abis.sql
@@ -13,4 +13,55 @@ WITH base AS (
     FROM
         {{ ref('silver__relevant_contracts') }}
     WHERE
-        total_interaction_count >= 100 {# {% if is_incremental() %} EXCEPT SELECT contract_address FROM {{ this }} WHERE abi_data :data :result :: STRING <> '{{ api_error_message }}' -- change to where the abi_data:data... or abi_data: data result ... {% endif %} LIMIT 5 ), all_contracts AS ( SELECT contract_address FROM base UNION SELECT contract_address FROM {{ ref('_retry_abis') }} #} ), row_nos AS ( SELECT contract_address, ROW_NUMBER() over ( ORDER BY contract_address ) AS row_no FROM all_contracts ), batched AS ({% for item in range(15) %} SELECT rn.contract_address, live.udf_api('GET', CONCAT('{{GLOBAL_API_URL}}', rn.contract_address, '&apikey=', '{{GLOBAL_API_SECRET_PATH}}'),{ 'User-Agent': 'FlipsideStreamline' },{}) AS abi_data, SYSDATE() AS _inserted_timestamp FROM row_nos rn WHERE row_no = {{ item }} {% if not loop.last %} UNION ALL {% endif %} {% endfor %}) SELECT contract_address, abi_data, _inserted_timestamp FROM batched
+        total_interaction_count >= 100
+
+{% if is_incremental() %}
+EXCEPT
+SELECT
+    contract_address
+FROM
+    {{ this }}
+WHERE
+    abi_data :data :result :: STRING <> '{{ api_error_message }}'
+{% endif %}
+LIMIT
+    5
+), all_contracts AS (
+    SELECT
+        contract_address
+    FROM
+        base
+    UNION
+    SELECT
+        contract_address
+    FROM
+        {{ ref('_retry_abis') }}
+),
+row_nos AS (
+    SELECT
+        contract_address,
+        ROW_NUMBER() over (
+            ORDER BY
+                contract_address
+        ) AS row_no
+    FROM
+        all_contracts
+),
+batched AS ({% for item in range(15) %}
+SELECT
+    rn.contract_address, live.udf_api('GET', CONCAT('{{GLOBAL_API_URL}}', rn.contract_address, '&apikey=', '{{GLOBAL_API_SECRET_PATH}}'),{ 'User-Agent': 'FlipsideStreamline' },{}) AS abi_data, SYSDATE() AS _inserted_timestamp
+FROM
+    row_nos rn
+WHERE
+    row_no = {{ item }}
+
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+{% endfor %})
+SELECT
+    contract_address,
+    abi_data,
+    _inserted_timestamp
+FROM
+    batched
