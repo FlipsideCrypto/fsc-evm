fbda92335f472a36ed5e5ccd74db400e95f28ba8
sam <sam@flipsidecrypto.com>
2024-10-16 14:31:42 +0800

    change source

--- models/core_package/silver/silver__created_contracts.sql
+++ models/core_package/silver/silver__created_contracts.sql
@@ -13,7 +13,7 @@ SELECT
     to_address AS created_contract_address,
     from_address AS creator_address,
     input AS created_contract_input,
-    _inserted_timestamp,
+    inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(
         ['to_address']
     ) }} AS created_contracts_id,
@@ -21,19 +21,19 @@ SELECT
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
-    {{ ref('silver__traces') }}
+    {{ ref('core__fact_traces') }}
 WHERE
     TYPE ILIKE 'create%'
     AND to_address IS NOT NULL
     AND input IS NOT NULL
     AND input != '0x'
-    AND tx_status = 'SUCCESS'
-    AND trace_status = 'SUCCESS'
+    AND tx_succeeded
+    AND trace_succeeded
 
 {% if is_incremental() %}
-AND _inserted_timestamp >= (
+AND inserted_timestamp >= (
     SELECT
-        MAX(_inserted_timestamp) - INTERVAL '24 hours'
+        MAX(inserted_timestamp) - INTERVAL '24 hours'
     FROM
         {{ this }}
 )
@@ -41,7 +41,7 @@ AND _inserted_timestamp >= (
 
 qualify(ROW_NUMBER() over(PARTITION BY created_contract_address
 ORDER BY
-    _inserted_timestamp DESC)) = 1
+    inserted_timestamp DESC)) = 1
 UNION ALL
 SELECT
     0 AS block_number,
