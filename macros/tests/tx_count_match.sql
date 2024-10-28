29e8850474e7d39bbf2a69b9e9e685b1040c76f0
Austin <austin@flipsidecrypto.com>
2024-10-28 12:16:53 -0400

    add tx count match

---
+++ macros/tests/tx_count_match.sql
@@ -0,0 +1,24 @@
+{% test tx_count_match(model, comparison_model) %}
+
+with count_txs as (
+    select 
+        block_number,
+        count(*) as record_count
+    from {{ model }}
+    group by all
+),
+
+block_txs as (
+    select
+        block_number,
+        tx_count as expected_count
+    from {{ comparison_model }}
+)
+select
+    block_number,
+    record_count as actual_count,
+    expected_count
+from block_txs
+left join count_txs using (block_number)
+where record_count != expected_count or expected_count is null
+{% endtest %}
