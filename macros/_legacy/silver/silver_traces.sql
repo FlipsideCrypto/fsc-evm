bbc4d2ff1192ade744a3dda27ebbedc7e5d3ecb8
drethereum <trevor.wenokur@gmail.com>
2024-10-29 14:41:04 -0600

    revert silver.traces macro changes

--- macros/_legacy/silver/silver_traces.sql
+++ macros/_legacy/silver/silver_traces.sql
@@ -27,7 +27,7 @@
 
 {% if is_incremental() and not full_reload_mode %}
 {{ ref(
-    schema_name ~ '__traces'
+    schema_name ~ '__streamline_traces'
 ) }}
 WHERE
     _inserted_timestamp >= (
@@ -42,7 +42,7 @@ WHERE
 
     {% elif is_incremental() and full_reload_mode %}
     {{ ref(
-        schema_name ~ '__traces_fr'
+        schema_name ~ '__streamline_fr_traces'
     ) }}
 WHERE
     {% if use_partition_key %}
@@ -78,7 +78,7 @@ WHERE
     {% endif %}
 {% else %}
     {{ ref(
-        schema_name ~ '__traces_fr'
+        schema_name ~ '__streamline_fr_traces'
     ) }}
 WHERE
     {% if use_partition_key %}
