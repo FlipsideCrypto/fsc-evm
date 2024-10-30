791f7eecb118a44678e68f4fd1cf549439de55f6
Austin <austin@flipsidecrypto.com>
2024-10-30 14:52:15 -0400

    tags

--- models/decoder_package/abis/silver/silver__flat_event_abis.sql
+++ models/decoder_package/abis/silver/silver__flat_event_abis.sql
@@ -3,8 +3,8 @@
     incremental_strategy = 'delete+insert',
     unique_key = "contract_address",
     cluster_by = "_inserted_timestamp::date",
-    tags = ['silver_abis'],
-    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY (contract_address)"
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY (contract_address)",
+    tags = ['silver_abis']
 ) }}
 
 WITH abi_base AS (
