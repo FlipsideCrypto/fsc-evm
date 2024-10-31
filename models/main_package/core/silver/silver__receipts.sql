2a3e26aed2d5319b0cb86d00ae0bb65f6190653a
drethereum <trevor.wenokur@gmail.com>
2024-10-31 13:33:54 -0600

    revert

--- models/main_package/core/silver/silver__receipts.sql
+++ models/main_package/core/silver/silver__receipts.sql
@@ -2,6 +2,7 @@
 {% set silver_full_refresh = var('SILVER_FULL_REFRESH', false) %}
 {% set unique_key = "tx_hash" if uses_receipts_by_hash else "block_number" %}
 {% set source_name = 'RECEIPTS_BY_HASH' if uses_receipts_by_hash else 'RECEIPTS' %}
+{% set post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(tx_hash)" if uses_receipts_by_hash else "" %}
 
 -- depends_on: {{ ref('bronze__' ~ source_name.lower()) }}
 
@@ -11,7 +12,7 @@
     incremental_strategy = 'delete+insert',
     unique_key = unique_key,
     cluster_by = ['modified_timestamp::DATE','partition_key'],
-    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(tx_hash)",
+    post_hook = post_hook,
     incremental_predicates = [fsc_evm.standard_predicate()],
     full_refresh = silver_full_refresh,
     tags = ['silver_core']
@@ -24,7 +25,7 @@
     incremental_strategy = 'delete+insert',
     unique_key = unique_key,
     cluster_by = ['modified_timestamp::DATE','partition_key'],
-    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(tx_hash)",
+    post_hook = post_hook,
     incremental_predicates = [fsc_evm.standard_predicate()],
     tags = ['silver_core']
 ) }}
@@ -39,7 +40,7 @@ WITH bronze_receipts AS (
             tx_hash,
             DATA:result AS receipts_json,
         {% else %}
-            receipts_json :transactionHash :: STRING AS tx_hash,
+            array_index,
             DATA AS receipts_json,
         {% endif %}
         _inserted_timestamp
@@ -70,12 +71,24 @@ WITH bronze_receipts AS (
 SELECT 
     block_number,
     partition_key,
-    tx_hash,
+    {% if uses_receipts_by_hash %}
+        tx_hash,
+    {% else %}
+        array_index,
+    {% endif %}
     receipts_json,
     _inserted_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['block_number','tx_hash']) }} AS receipts_id,
+    {% if uses_receipts_by_hash %}
+        {{ dbt_utils.generate_surrogate_key(['block_number','tx_hash']) }} AS receipts_id,
+    {% else %}
+        {{ dbt_utils.generate_surrogate_key(['block_number','array_index']) }} AS receipts_id,
+    {% endif %}
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM bronze_receipts
+{% if uses_receipts_by_hash %}
 QUALIFY ROW_NUMBER() OVER (PARTITION BY tx_hash ORDER BY block_number DESC, _inserted_timestamp DESC) = 1
+{% else %}
+QUALIFY ROW_NUMBER() OVER (PARTITION BY array_index ORDER BY block_number DESC, _inserted_timestamp DESC) = 1
+{% endif %}
