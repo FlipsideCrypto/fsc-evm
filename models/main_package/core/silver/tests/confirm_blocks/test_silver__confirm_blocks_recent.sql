5f27a111d353f1d687e2cb7aa6e949eb5e5e906a
Austin <austin@flipsidecrypto.com>
2024-11-01 12:01:52 -0400

    test

--- models/main_package/core/silver/tests/confirm_blocks/test_silver__confirm_blocks_recent.sql
+++ models/main_package/core/silver/tests/confirm_blocks/test_silver__confirm_blocks_recent.sql
@@ -8,10 +8,27 @@ SELECT
 FROM
     {{ ref('silver__confirm_blocks') }}
 WHERE
-    modified_timestamp > DATEADD('hour',-12,sysdate()) and 
-    partition_key > (
+    modified_timestamp > DATEADD(
+        'hour' (
+            SELECT
+                IFF(
+                    DATEDIFF('hour', MIN(modified_timestamp), SYSDATE()) <= (
+                        24 * 6
+                    ),
+                    -12,
+                    -24 * 5
+                )
+            FROM
+                {{ ref('silver__confirm_blocks') }}
+        ),
+        SYSDATE()
+    )
+    AND partition_key > (
         SELECT
-            round(block_number, -3) AS block_number
+            ROUND(
+                block_number,
+                -3
+            ) AS block_number
         FROM
             {{ ref('_block_lookback') }}
     )
