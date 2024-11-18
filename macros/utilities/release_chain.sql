2ab730e2763cc9ba746251c23c7c1eb196502bcc
Austin <austin@flipsidecrypto.com>
2024-11-18 13:20:20 -0500

    stash

---
+++ macros/utilities/release_chain.sql
@@ -0,0 +1,14 @@
+{% macro release_chain(schema_name, role_name) %}
+
+{% set prod_db_name = var('GLOBAL_PROD_DB_NAME', '') | upper %}
+
+    {% if target.database | upper == prod_db_name and target.name == 'prod' %}
+        {% do run_query("GRANT USAGE ON DATABASE " ~ prod_db_name ~ " TO ROLE " ~ role_name ~ " COPY CURRENT GRANTS;") %}
+        {% do run_query("GRANT USAGE ON SCHEMA " ~ prod_db_name ~ "." ~ schema_name ~ " TO ROLE " ~ role_name ~ " COPY CURRENT GRANTS;") %}
+        {% do run_query("GRANT SELECT ON ALL TABLES IN SCHEMA " ~ prod_db_name ~ "." ~ schema_name ~ " TO ROLE " ~ role_name ~ " COPY CURRENT GRANTS;") %}
+        {% do run_query("GRANT SELECT ON ALL VIEWS IN SCHEMA " ~ prod_db_name ~ "." ~ schema_name ~ " TO ROLE " ~ role_name ~ " COPY CURRENT GRANTS;") %}
+        {% do run_query("GRANT SELECT ON ALL FUTURE TABLES IN SCHEMA " ~ schema_name ~ " TO ROLE " ~ role_name ~ ";") %}
+        {% do run_query("GRANT SELECT ON ALL FUTURE VIEWS IN SCHEMA " ~ schema_name ~ " TO ROLE " ~ role_name ~ ";") %}
+    {% endif %}
+
+{% endmacro %}
