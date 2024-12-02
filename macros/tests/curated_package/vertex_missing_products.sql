4a93c2a151017568dbdf0ffc822346428a29ad8b
mattromano <mattromano88@gmail.com>
2024-12-02 12:53:06 -0800

    create specific market stats filter

--- macros/tests/curated_package/vertex_missing_products.sql
+++ macros/tests/curated_package/vertex_missing_products.sql
@@ -8,7 +8,22 @@ with recent_records as (
     from 
         {{ model }}
     where 
-        modified_timestamp >= SYSDATE() - INTERVAL '7 days'
+        {% if model == 'silver__vertex_market_stats' %}
+            product_type = 'perp' AND product_id <> 0 AND BASE_VOLUME_24H > 0 AND product_id in (
+                select product_id
+                from (
+                    select product_id,
+                    min(hour) as min_hour 
+                    from {{this}}
+                    group by 1 
+                    having min_hour <= SYSDATE() - INTERVAL '2 days'
+                )
+            )
+            AND 
+                modified_timestamp >= SYSDATE() - INTERVAL '7 days'
+        {% else %}
+            modified_timestamp >= SYSDATE() - INTERVAL '7 days'
+        {% endif %}
 ),
 
 invalid_product_ids as (
