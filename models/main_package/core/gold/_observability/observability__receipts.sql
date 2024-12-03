4af1168f928fc2ae60f07e25d8f21ecf6ba1e666
sam <sam@flipsidecrypto.com>
2024-12-03 20:08:56 +0800

    add incremental

--- models/main_package/core/gold/_observability/observability__receipts.sql
+++ models/main_package/core/gold/_observability/observability__receipts.sql
@@ -5,6 +5,7 @@
     tags = ['observability']
 ) }}
 
+{% if is_incremental() %}
 WITH lookback AS (
 
     SELECT
@@ -35,10 +36,11 @@ WITH lookback AS (
         )
 ),
 base AS (
+    {% else %}
+        WITH base AS (
+    {% endif %}
     SELECT
-        block_number,
-        block_timestamp,
-        tx_hash
+        block_number, block_timestamp, tx_hash
     FROM
         {{ ref('core__fact_transactions') }}
     WHERE
@@ -46,13 +48,11 @@ base AS (
 
 {% if is_incremental() %}
 AND block_number >= (
-    SELECT
-        block_number
-    FROM
-        lookback
-)
-{% endif %}
-),
+SELECT
+    block_number
+FROM
+    lookback)
+{% endif %}),
 summary_stats AS (
     SELECT
         MIN(block_number) AS min_block,
