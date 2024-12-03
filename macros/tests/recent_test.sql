fbe86a383a8df6ea5a595ed765e54af638eb87ae
mattromano <mattromano88@gmail.com>
2024-12-03 15:38:08 -0800

    more tests

--- macros/tests/recent_test.sql
+++ macros/tests/recent_test.sql
@@ -1,22 +1,11 @@
-{% macro recent_test(model, test_sql) %}
-    {% if var('RECENT_TEST_DAYS', 3) is none %}
-        {# Run test on all data #}
-        {{ test_sql }}
-    {% else %}
-        {# Add timestamp filter to the original test SQL #}
-        {% set date_filter %}
-            AND block_timestamp >= DATEADD('day', -{{ var('RECENT_TEST_DAYS', 3) }}, CURRENT_TIMESTAMP())
-        {% endset %}
-
-        {# Insert the date filter before the first GROUP BY or HAVING or at the end if neither exists #}
-        {% if test_sql is match('.*GROUP BY.*', flags='is') %}
-            {% set final_sql = test_sql | replace('GROUP BY', date_filter ~ ' GROUP BY') %}
-        {% elif test_sql is match('.*HAVING.*', flags='is') %}
-            {% set final_sql = test_sql | replace('HAVING', date_filter ~ ' HAVING') %}
-        {% else %}
-            {% set final_sql = test_sql ~ ' ' ~ date_filter %}
-        {% endif %}
-        
-        {{ final_sql }}
-    {% endif %}
-{% endmacro %}
+{% test recent_test(model, column_name="BLOCK_TIMESTAMP", days=1) %}
+    {{ config(severity='error') }}
+    
+    with latest_data as (
+        select max({{ column_name }}) as max_ts
+        from {{ model }}
+    )
+    select *
+    from latest_data
+    where max_ts < dateadd('day', -{{ days }}, current_timestamp())
+{% endtest %}
