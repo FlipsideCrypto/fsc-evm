14f982d97d97c3eefbbbe103b0b62572104249c8
mattromano <mattromano88@gmail.com>
2024-12-03 15:17:57 -0800

    macro update

--- macros/tests/recent_test.sql
+++ macros/tests/recent_test.sql
@@ -5,11 +5,17 @@
     {% else %}
         {# Add timestamp filter to the original test SQL #}
         {% set date_filter %}
-            WHERE block_timestamp >= DATEADD('day', -{{ var('RECENT_TEST_DAYS', 3) }}, CURRENT_TIMESTAMP())
+            AND block_timestamp >= DATEADD('day', -{{ var('RECENT_TEST_DAYS', 3) }}, CURRENT_TIMESTAMP())
         {% endset %}
 
-        {# Insert the WHERE clause after the model reference #}
-        {% set final_sql = test_sql | replace(model, model ~ ' ' ~ date_filter) %}
+        {# Insert the date filter before the first GROUP BY or HAVING or at the end if neither exists #}
+        {% if test_sql is match('.*GROUP BY.*', flags='is') %}
+            {% set final_sql = test_sql | replace('GROUP BY', date_filter ~ ' GROUP BY') %}
+        {% elif test_sql is match('.*HAVING.*', flags='is') %}
+            {% set final_sql = test_sql | replace('HAVING', date_filter ~ ' HAVING') %}
+        {% else %}
+            {% set final_sql = test_sql ~ ' ' ~ date_filter %}
+        {% endif %}
         
         {{ final_sql }}
     {% endif %}
