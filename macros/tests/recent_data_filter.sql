0a255ec285600943c23e7fc8f4fbe123880b0395
mattromano <mattromano88@gmail.com>
2024-12-03 16:15:06 -0800

    new method

--- macros/tests/recent_data_filter.sql
+++ macros/tests/recent_data_filter.sql
@@ -1,5 +1,17 @@
 {% macro recent_data_filter() %}
-  {% if 'curated' in config.get('tags', []) %}
-    and BLOCK_TIMESTAMP >= dateadd('day', -1, current_timestamp())
-  {% endif %}
-{% endmacro %} 
+    {% if 'curated' in model.config.get('tags', []) %}
+        {% set original_sql = model.compiled_sql %}
+        {% if 'where' in original_sql.lower() %}
+            {% set modified_sql = original_sql | replace('where', 'where BLOCK_TIMESTAMP >= dateadd(day, -3, current_timestamp()) and') %}
+        {% else %}
+            {% set modified_sql = original_sql ~ ' where BLOCK_TIMESTAMP >= dateadd(day, -3, current_timestamp())' %}
+        {% endif %}
+        {{ return(modified_sql) }}
+    {% else %}
+        {{ return(original_sql) }}
+    {% endif %}
+{% endmacro %}
+
+{% on_run_start %}
+    {{ config.set_test_compile_hook(recent_data_filter) }}
+{% endon_run_start %}
