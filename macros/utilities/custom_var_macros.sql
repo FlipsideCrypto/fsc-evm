e86e7b29c707198da8ec1fd9ab704b40de7d08fa
drethereum <trevor.wenokur@gmail.com>
2024-12-11 17:57:08 -0700

    updates for mapping

--- macros/utilities/custom_var_macros.sql
+++ macros/utilities/custom_var_macros.sql
@@ -1,18 +1,37 @@
 {% macro get_vars(var_name, default=none) %}
+    {% if var(var_name, none) is not none %}
+        {{ return(var(var_name)) }}
+    {% endif %}
+
     {% set query %}
-        SELECT value 
+        SELECT 
+            variable_name,
+            parent_key,
+            variable_name as key,
+            value
         FROM {{ ref('silver__variables_seed_test') }}
         WHERE variable_name = '{{ var_name }}'
-        LIMIT 1
+           OR parent_key = '{{ var_name }}'
+        ORDER BY variable_name
     {% endset %}
     
-    {% set results = run_query(query) %}
-    
     {% if execute %}
+        {% set results = run_query(query) %}
+        
         {% if results.rows | length == 0 %}
             {{ return(default) }}
         {% endif %}
-        {{ return(results.columns[0].values()[0]) }}
+
+        {# Check if this is a mapping type (has child rows) #}
+        {% if results.rows[0][1] is none or results.rows[0][1] == '' %}
+            {{ return(results.rows[0][3]) }}  {# Return value #}
+        {% else %}
+            {% set mapping = {} %}
+            {% for row in results.rows %}
+                {% do mapping.update({row[0]: row[3]}) %}  {# variable_name: value #}
+            {% endfor %}
+            {{ return(mapping) }}
+        {% endif %}
     {% else %}
         {{ return('') }}
     {% endif %}
