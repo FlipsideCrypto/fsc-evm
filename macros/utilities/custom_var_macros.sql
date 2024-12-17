2510e02ad1c4c2c8242fa1cca0ed112a32895389
drethereum <trevor.wenokur@gmail.com>
2024-12-17 13:43:51 -0700

    comments

--- macros/utilities/custom_var_macros.sql
+++ macros/utilities/custom_var_macros.sql
@@ -1,8 +1,10 @@
-{% macro get_vars(var_name, default=none) %}
+{% macro get_var(var_name, default=none) %}
+    {# Check if variable exists in dbt's built-in var() function. If it does, return the value. #}
     {% if var(var_name, none) is not none %}
         {{ return(var(var_name)) }}
     {% endif %}
 
+    {# Query to get variable value from custom variables table #}
     {% set query %}
         SELECT 
             variable_name,
@@ -15,24 +17,29 @@
         ORDER BY variable_name
     {% endset %}
     
+    {# Only execute query during actual dbt run, not during compile #}
     {% if execute %}
         {% set results = run_query(query) %}
         
+        {# If no results found, return the default value #}
         {% if results.rows | length == 0 %}
             {{ return(default) }}
         {% endif %}
 
-        {# Check if this is a mapping type (has child rows) #}
+        {# Check if this is a simple variable (no parent key) or a mapping (has parent key) #}
         {% if results.rows[0][1] is none or results.rows[0][1] == '' %}
-            {{ return(results.rows[0][3]) }}  {# Return value #}
+            {# For simple variable, return the value directly #}
+            {{ return(results.rows[0][3]) }}
         {% else %}
+            {# For mapping type, build a dictionary of all child values #}
             {% set mapping = {} %}
             {% for row in results.rows %}
-                {% do mapping.update({row[0]: row[3]}) %}  {# variable_name: value #}
+                {% do mapping.update({row[0]: row[3]}) %}
             {% endfor %}
             {{ return(mapping) }}
         {% endif %}
     {% else %}
+        {# During compile phase, return empty string #}
         {{ return('') }}
     {% endif %}
 {% endmacro %}
