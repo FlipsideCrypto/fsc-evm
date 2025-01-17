f69a3ce45d43053c44f427ca83882ce598286889
drethereum <trevor.wenokur@gmail.com>
2025-01-17 16:59:13 -0700

    replace logging macros to test

--- models/main_package/prices/silver/silver__complete_token_asset_metadata.sql
+++ models/main_package/prices/silver/silver__complete_token_asset_metadata.sql
@@ -1,18 +1,5 @@
 {# Log configuration details #}
-{%- if flags.WHICH == 'compile' and execute -%}
-
-    {% set config_log = '\n' %}
-    {% set config_log = config_log ~ '\n=== DBT Model Config ===\n'%}
-    {% set config_log = config_log ~ '\n{{ config (\n' %}
-    {% set config_log = config_log ~ '    materialized = "' ~ config.get('materialized') ~ '",\n' %}
-    {% set config_log = config_log ~ '    incremental_strategy = "' ~ config.get('incremental_strategy') ~ '",\n' %}
-    {% set config_log = config_log ~ '    unique_key = "' ~ config.get('unique_key') ~ '",\n' %}
-    {% set config_log = config_log ~ '    tags = ' ~ config.get('tags') ~ '\n' %}
-    {% set config_log = config_log ~ ') }}\n' %}
-    {{ log(config_log, info=True) }}
-    {{ log("", info=True) }}
-
-{%- endif -%}
+{{ log_model_config() }}
 
 {# Set up dbt configuration #}
 {{ config(
