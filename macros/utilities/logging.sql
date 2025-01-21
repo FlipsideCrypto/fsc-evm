fbc0d40bf4ee4c1aea925b9d9b816ad320765c4b
drethereum <trevor.wenokur@gmail.com>
2025-01-21 15:35:58 -0700

    remove space

--- macros/utilities/logging.sql
+++ macros/utilities/logging.sql
@@ -6,19 +6,18 @@ DBT Model Config:
 {{ model.config | tojson(indent=2) }}
 */
     
-{% if streamline_params %}
-/*
-Streamline Parameters: 
-{{ streamline_params | tojson(indent=2) }}
-*/
-{% endif %}
-
 {% if default_vars %}
 /*
 Default Variables:
 {{ default_vars | tojson(indent=2) }}
 */
+{% endif %}
 
+{% if streamline_params %}
+/*
+Streamline Parameters: 
+{{ streamline_params | tojson(indent=2) }}
+*/
 {% endif %}
 
 /*
