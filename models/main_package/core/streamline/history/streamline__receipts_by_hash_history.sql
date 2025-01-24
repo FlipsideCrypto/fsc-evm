28630b636aeefae979c4766407d4bb21f0a8ac5a
drethereum <trevor.wenokur@gmail.com>
2025-01-24 11:06:19 -0700

    updated logic

--- models/main_package/core/streamline/history/streamline__receipts_by_hash_history.sql
+++ models/main_package/core/streamline/history/streamline__receipts_by_hash_history.sql
@@ -43,7 +43,6 @@
 ) }}
 
 {# Main query starts here #}
-{# Start by invoking LQ for the last hour of blocks #}
 
 WITH 
 {% if not new_build %}
@@ -53,111 +52,18 @@ WITH
     ),
 {% endif %}
 
-numbered_blocks AS (
-
-    SELECT
-        block_number_hex,
-        block_number,
-        ROW_NUMBER() over (
-            ORDER BY
-                block_number
-        ) AS row_num
-    FROM
-        (
-            SELECT
-                *
-            FROM
-                {{ ref('streamline__blocks') }}
-            ORDER BY
-                block_number DESC
-            LIMIT
-                {{ var('GLOBAL_BLOCKS_PER_HOUR', sql_limit) }}
-        )
-), batched_blocks AS (
-    SELECT
-        block_number_hex,
-        block_number,
-        100 AS rows_per_batch,
-        CEIL(
-            row_num / rows_per_batch
-        ) AS batch_number,
-        MOD(
-            row_num - 1,
-            rows_per_batch
-        ) + 1 AS row_within_batch
-    FROM
-        numbered_blocks
-),
-batched_calls AS (
-    SELECT
-        batch_number,
-        ARRAY_AGG(
-            utils.udf_json_rpc_call(
-                'eth_getBlockByNumber',
-                [block_number_hex, false]
-            )
-        ) AS batch_request
-    FROM
-        batched_blocks
-    GROUP BY
-        batch_number
-),
-rpc_requests AS (
-    SELECT
-        live.udf_api(
-            'POST',
-            '{{ node_url }}',
-            OBJECT_CONSTRUCT(
-                'Content-Type',
-                'application/json',
-                'fsc-quantum-state',
-                'livequery'
-            ),
-            batch_request,
-            '{{ node_secret_path }}'
-        ) AS resp
-    FROM
-        batched_calls
-),
-blocks AS (
-    SELECT
-        utils.udf_hex_to_int(
-            VALUE :result :number :: STRING
-        ) :: INT AS block_number,
-        VALUE :result :transactions AS tx_hashes
-    FROM
-        rpc_requests,
-        LATERAL FLATTEN (
-            input => resp :data
-        )
-),
-flat_tx_hashes AS (
-    SELECT
-        block_number,
-        VALUE :: STRING AS tx_hash
-    FROM
-        blocks,
-        LATERAL FLATTEN (
-            input => tx_hashes
-        )
-),
 to_do AS (
     SELECT 
         block_number,
         tx_hash
-    FROM (
-        SELECT
-            block_number,
-            tx_hash
-        FROM
-            flat_tx_hashes
-        UNION ALL
-        SELECT
-            block_number,
-            tx_hash
-        FROM
-            {{ ref("core__fact_transactions") }}
-    )
+    FROM {{ ref("core__fact_transactions") }}
+    WHERE 
+        (block_number IS NOT NULL 
+        AND tx_hash IS NOT NULL)
+    {% if not new_build %}
+        AND block_number <= (SELECT block_number FROM last_3_days)
+    {% endif %}
+
     EXCEPT
 
     SELECT
