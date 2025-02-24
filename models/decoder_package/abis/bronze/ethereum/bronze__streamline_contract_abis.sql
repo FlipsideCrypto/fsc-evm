81eacac24632a420424eedec726fa361bc8257e2
sam <sam@flipsidecrypto.com>
2025-02-24 12:24:16 +0800

    update ethereum

--- models/decoder_package/abis/bronze/ethereum/bronze__streamline_contract_abis.sql
+++ models/decoder_package/abis/bronze/ethereum/bronze__streamline_contract_abis.sql
@@ -2,6 +2,7 @@
     materialized = 'view'
 ) }}
 {{ fsc_evm.streamline_external_table_query(
-    model = "contract_abis_v2",
+    source_name = 'contract_abis',
+    source_version = 'v2',
     partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER)"
 ) }}
--- models/decoder_package/abis/bronze/ethereum/bronze__streamline_fr_contract_abis_v2.sql
+++ models/decoder_package/abis/bronze/ethereum/bronze__streamline_fr_contract_abis_v2.sql
@@ -1,7 +1,8 @@
 {{ config (
     materialized = 'view'
 ) }}
-{{ fsc_evm.streamline_external_table_fr_query(
-    model = "contract_abis_v2",
+{{ fsc_evm.streamline_external_table_query_fr(
+    source_name = 'contract_abis',
+    source_version = 'v2',
     partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER )"
 ) }}
