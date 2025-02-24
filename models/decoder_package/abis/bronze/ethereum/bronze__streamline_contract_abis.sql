82b5116262a084a5bd2fab1114aef88a31829b98
sam <sam@flipsidecrypto.com>
2025-02-24 22:01:20 +0800

    delete test files

--- models/decoder_package/abis/bronze/ethereum/bronze__streamline_contract_abis.sql
+++
@@ -1,8 +0,0 @@
-{{ config (
-    materialized = 'view'
-) }}
-{{ fsc_evm.streamline_external_table_query(
-    source_name = 'contract_abis',
-    source_version = 'v2',
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER)"
-) }}
