24e7ed8cb8bb74b41df21ba55127467284d8f4dc
Austin <austin@flipsidecrypto.com>
2025-02-24 15:33:16 -0500

    fields

--- models/main_package/utils/utils__rpc_settings.sql
+++ models/main_package/utils/utils__rpc_settings.sql
@@ -13,18 +13,18 @@ select
     array_contains('blobGasUsed'::VARIANT, blocks_fields) as blocks_has_blob_gas_used,
     array_contains('parentBeaconBlockRoot'::VARIANT, blocks_fields) as blocks_has_parent_beacon_block_root,
     array_contains('withdrawals'::VARIANT, blocks_fields) as blocks_has_withdrawals,
-    array_contains('accessList'::VARIANT, txs_fields) as tx_has_access_list,
-    array_contains('maxFeePerGas'::VARIANT, txs_fields) as tx_has_max_fee_per_gas,
-    array_contains('maxPriorityFeePerGas'::VARIANT, txs_fields) as tx_has_max_priority_fee_per_gas,
-    array_contains('blobGasPrice'::VARIANT, txs_fields) as tx_has_blob_gas_price,
-    array_contains('sourceHash'::VARIANT, txs_fields) as tx_has_source_hash,
-    array_contains('mint'::VARIANT, txs_fields) as tx_has_mint,
-    array_contains('ethValue'::VARIANT, txs_fields) as tx_has_eth_value,
-    array_contains('yParity'::VARIANT, txs_fields) as tx_has_y_parity,
+    array_contains('accessList'::VARIANT, transactions_fields) as tx_has_access_list,
+    array_contains('maxFeePerGas'::VARIANT, transactions_fields) as tx_has_max_fee_per_gas,
+    array_contains('maxPriorityFeePerGas'::VARIANT, transactions_fields) as tx_has_max_priority_fee_per_gas,
+    array_contains('blobGasPrice'::VARIANT, transactions_fields) as tx_has_blob_gas_price,
+    array_contains('sourceHash'::VARIANT, transactions_fields) as tx_has_source_hash,
+    array_contains('mint'::VARIANT, transactions_fields) as tx_has_mint,
+    array_contains('ethValue'::VARIANT, transactions_fields) as tx_has_eth_value,
+    array_contains('yParity'::VARIANT, transactions_fields) as tx_has_y_parity,
     array_contains('l1Fee'::VARIANT, receipts_fields) as tx_has_l1_columns,
     array_contains('l1FeeScalar'::VARIANT, receipts_fields) as tx_has_l1_tx_fee_calc,
     array_contains('l1BlobBaseFee'::VARIANT, receipts_fields) as tx_has_blob_base_fee,
-    array_contains('maxFeePerGas'::VARIANT, txs_fields) as tx_has_eip_1559
+    array_contains('maxFeePerGas'::VARIANT, transactions_fields) as tx_has_eip_1559
 from MANTLE_DEV.SILVER.BLOCKCHAIN_COMPATIBILITY_LOGS
 where blockchain = 'swell' -- global prod db name 
 qualify row_number() over (partition by blockchain order by inserted_at desc) = 1
