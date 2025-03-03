2532e25e492e07148036269d7b0c1a3973a78e88
Austin <austin@flipsidecrypto.com>
2025-03-03 15:51:46 -0500

    tag

--- models/main_package/core/gold/core__dim_contracts.sql
+++ models/main_package/core/gold/core__dim_contracts.sql
@@ -3,7 +3,7 @@
     unique_key = 'address',
     merge_exclude_columns = ["inserted_timestamp"],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(address, symbol, name), SUBSTRING(address, symbol, name)",
-    tags = ['gold_core']
+    tags = ['gold_core', 'return_vars']
 ) }}
 
 SELECT
