09f329df10f13baac33f8c10f9159287a1f55602
Austin <austin@flipsidecrypto.com>
2025-03-04 12:00:36 -0500

    v1

--- macros/config/get_config_v1.sql
+++ macros/config/get_config_v1.sql
@@ -1,44 +1,28 @@
-{% macro get_config_v1(key, default='') %}
+{% macro get_config(key, default='') %}
     {# 
     Enhanced version that:
-    1. Auto-detects the current project name from the file path
-    2. Supports nested configs under chain-specific models
+    1. Uses the project name from dbt_project.yml
+    2. Supports nested configs under chain-specific names
     3. Falls back to global defaults when a specific chain config isn't available
     #}
     
-    {# Extract project name from path #}
-    {% set project_path = this.path %}
-    {% set project_name = '' %}
-    
-    {% if project_path %}
-        {# Extract the project name from the path - typically in format "path/to/[project-name]-models/..." #}
-        {% set path_parts = project_path.split('/') %}
-        {% for part in path_parts %}
-            {% if part.endswith('-models') %}
-                {% set project_name = part.split('-models')[0] %}
-                {% break %}
-            {% endif %}
-        {% endfor %}
-    {% endif %}
-    
-    {# If we couldn't detect the project name, default to 'mantle' #}
-    {% if project_name == '' %}
-        {% set project_name = 'mantle' %}
-    {% endif %}
+    {# Extract chain name from the project name #}
+    {% set project_name = project_name %}
+    {% set chain_name = project_name.split('_')[0] if '_' in project_name else project_name %}
     
     {# Define our nested configuration structure #}
     {% set config = {
-        'mantle-models': {
+        'mantle': {
             'GLOBAL_CHAIN_NETWORK': 'mantle',
             'MAIN_SL_BLOCKS_PER_HOUR': 1800,
             'GLOBAL_PROD_DB_NAME': 'mantle'
         },
-        'swell-models': {
+        'swell': {
             'GLOBAL_CHAIN_NETWORK': 'swell',
             'MAIN_SL_BLOCKS_PER_HOUR': 1500,
             'GLOBAL_PROD_DB_NAME': 'swell'
         },
-        'ethereum-models': {
+        'ethereum': {
             'GLOBAL_CHAIN_NETWORK': 'ethereum',
             'MAIN_SL_BLOCKS_PER_HOUR': 300,
             'GLOBAL_PROD_DB_NAME': 'ethereum'
@@ -50,11 +34,9 @@
         }
     } %}
     
-    {# Try to get the value from project-specific config #}
-    {% set project_key = project_name + '-models' %}
-    
-    {% if project_key in config and key in config[project_key] %}
-        {{ return(config[project_key][key]) }}
+    {# Try to get the value from chain-specific config #}
+    {% if chain_name in config and key in config[chain_name] %}
+        {{ return(config[chain_name][key]) }}
     {% elif key in config['global'] %}
         {# Fall back to global config if the key exists there #}
         {{ return(config['global'][key]) }}
