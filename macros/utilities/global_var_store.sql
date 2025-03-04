21e9a1e330ac1502f0902d58e683e89def97d84d
Austin <austin@flipsidecrypto.com>
2025-03-04 13:46:26 -0500

    a

--- macros/utilities/global_var_store.sql
+++ macros/utilities/global_var_store.sql
@@ -1,11 +1,15 @@
-{% macro init_global_vars() %}
-  {% if not adapter.config.has_node('global_vars') %}
-    {% do adapter.config.set_node('global_vars', return_vars()) %}
-    {% do log("Global variables initialized", info=true) %}
-  {% endif %}
+{% macro _get_global_vars() %}
+  {% do return(return_vars()) %}
 {% endmacro %}
 
 {% macro get_global_vars() %}
-  {{ init_global_vars() }}
-  {{ return(adapter.config.get_node('global_vars')) }}
+  {# Use the global dictionary to cache variable results #}
+  {% if not context.get('_cached_global_vars') %}
+    {% do context.update({'_cached_global_vars': _get_global_vars()}) %}
+    {% do log("Global variables initialized for the first time", info=true) %}
+  {% else %}
+    {% do log("Using cached global variables", info=true) %}
+  {% endif %}
+  
+  {% do return(context._cached_global_vars) %}
 {% endmacro %}
