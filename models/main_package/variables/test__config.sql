59091e6b393baeb9c2ef12bde7f72f9608af6c54
Austin <austin@flipsidecrypto.com>
2025-03-04 13:22:01 -0500

    test

--- models/main_package/variables/test__config.sql
+++ models/main_package/variables/test__config.sql
@@ -8,9 +8,9 @@
 
 WITH config_test AS (
   SELECT 
-    {{ vars.GLOBAL_CHAIN_NETWORK }} AS chain_network,
+    '{{ vars.MAIN_PRICES_NATIVE_SYMBOLS }}' AS NATIVE_SYMBOLS,
     {{ vars.MAIN_SL_BLOCKS_PER_HOUR }} AS blocks_per_hour,
-    {{ vars.GLOBAL_PROD_DB_NAME }} AS db_name
+    '{{ vars.GLOBAL_PROD_DB_NAME }}' AS db_name
 )
 
 SELECT * FROM config_test
