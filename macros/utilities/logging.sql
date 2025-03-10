5339f3da9955b31021c52a845eae3c40c0171d3e
drethereum <trevor.wenokur@gmail.com>
2025-03-10 10:42:13 -0600

    removed unused macros

--- macros/utilities/logging.sql
+++ macros/utilities/logging.sql
@@ -1,4 +1,4 @@
-{% macro log_model_details(vars=false, params=false) %}
+{% macro log_model_details() %}
 
 {%- if execute -%}
 /* 
@@ -6,28 +6,6 @@ DBT Model Config:
 {{ model.config | tojson(indent=2) }}
 */
     
-{% if vars is not false %}
-
-{% if get_var('GLOBAL_LOGGING_MODEL_LOGS_ENABLED', false) %}
-{{ log( vars | tojson(indent=2), info=True) }}
-{% endif %}
-/*
-Variables:
-{{ vars | tojson(indent=2) }}
-*/
-{% endif %}
-
-{% if params is not false %}
-
-{% if get_var('GLOBAL_LOGGING_MODEL_LOGS_ENABLED', false) %}
-{{ log( params | tojson(indent=2), info=True) }}
-{% endif %}
-/*
-Parameters: 
-{{ params | tojson(indent=2) }}
-*/
-{% endif %}
-
 /*
 Raw Code:
 {{ model.raw_code }}
