7f538e120486b977c5a5c89c59f9791cd40f1ded
drethereum <trevor.wenokur@gmail.com>
2025-03-10 10:08:09 -0600

    silver vars

--- models/main_package/core/silver/silver__receipts.sql
+++ models/main_package/core/silver/silver__receipts.sql
@@ -1,45 +1,27 @@
-{% set uses_receipts_by_hash = get_var('MAIN_CORE_RECEIPTS_BY_HASH_ENABLED', false) %}
-{% set silver_full_refresh = get_var('GLOBAL_SILVER_FR_ENABLED', false) %}
-{% set unique_key = "tx_hash" if uses_receipts_by_hash else "block_number" %}
-{% set source_name = 'RECEIPTS_BY_HASH' if uses_receipts_by_hash else 'RECEIPTS' %}
-{% set post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(tx_hash, block_number)" if uses_receipts_by_hash else "" %}
+{# Get variables #}
+{% set vars = return_vars() %}
 
 {# Log configuration details #}
 {{ log_model_details() }}
 
--- depends_on: {{ ref('bronze__' ~ source_name.lower()) }}
-
-{% if not silver_full_refresh %}
-{{ config (
-    materialized = "incremental",
-    incremental_strategy = 'delete+insert',
-    unique_key = unique_key,
-    cluster_by = ['modified_timestamp::DATE','partition_key'],
-    post_hook = post_hook,
-    incremental_predicates = [fsc_evm.standard_predicate()],
-    full_refresh = silver_full_refresh,
-    tags = ['silver_core']
-) }}
-
-{% else %}
+-- depends_on: {{ ref('bronze__' ~ vars.MAIN_CORE_SILVER_RECEIPTS_SOURCE_NAME.lower()) }}
 
 {{ config (
     materialized = "incremental",
     incremental_strategy = 'delete+insert',
-    unique_key = unique_key,
+    unique_key = vars.MAIN_CORE_SILVER_RECEIPTS_UNIQUE_KEY,
     cluster_by = ['modified_timestamp::DATE','partition_key'],
-    post_hook = post_hook,
+    post_hook = vars.MAIN_CORE_SILVER_RECEIPTS_POST_HOOK,
     incremental_predicates = [fsc_evm.standard_predicate()],
+    full_refresh = vars.GLOBAL_SILVER_FR_ENABLED,
     tags = ['silver_core']
 ) }}
-
-{% endif %}
 
 WITH bronze_receipts AS (
     SELECT 
         block_number,
         partition_key,
-        {% if uses_receipts_by_hash %}
+        {% if vars.MAIN_CORE_RECEIPTS_BY_HASH_ENABLED %}
             tx_hash,
             DATA:result AS receipts_json,
         {% else %}
@@ -49,21 +31,21 @@ WITH bronze_receipts AS (
         _inserted_timestamp
     FROM 
     {% if is_incremental() %}
-    {{ ref('bronze__' ~ source_name.lower()) }}
+    {{ ref('bronze__' ~ vars.MAIN_CORE_SILVER_RECEIPTS_SOURCE_NAME.lower()) }}
     WHERE _inserted_timestamp >= (
         SELECT 
             COALESCE(MAX(_inserted_timestamp), '1900-01-01'::TIMESTAMP) AS _inserted_timestamp
         FROM {{ this }}
     ) AND 
-    {% if uses_receipts_by_hash %}
+    {% if vars.MAIN_CORE_RECEIPTS_BY_HASH_ENABLED %}
         DATA:result IS NOT NULL
     {% else %}
         DATA IS NOT NULL
     {% endif %}
     {% else %}
-    {{ ref('bronze__' ~ source_name.lower() ~ '_fr') }}
+    {{ ref('bronze__' ~ vars.MAIN_CORE_SILVER_RECEIPTS_SOURCE_NAME.lower() ~ '_fr') }}
     WHERE 
-    {% if uses_receipts_by_hash %}
+    {% if vars.MAIN_CORE_RECEIPTS_BY_HASH_ENABLED %}
         DATA:result IS NOT NULL
     {% else %}
         DATA IS NOT NULL
@@ -74,14 +56,14 @@ WITH bronze_receipts AS (
 SELECT 
     block_number,
     partition_key,
-    {% if uses_receipts_by_hash %}
+    {% if vars.MAIN_CORE_RECEIPTS_BY_HASH_ENABLED %}
         tx_hash,
     {% else %}
         array_index,
     {% endif %}
     receipts_json,
     _inserted_timestamp,
-    {% if uses_receipts_by_hash %}
+    {% if vars.MAIN_CORE_RECEIPTS_BY_HASH_ENABLED %}
         {{ dbt_utils.generate_surrogate_key(['block_number','tx_hash']) }} AS receipts_id,
     {% else %}
         {{ dbt_utils.generate_surrogate_key(['block_number','array_index']) }} AS receipts_id,
@@ -90,7 +72,7 @@ SELECT
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM bronze_receipts
-{% if uses_receipts_by_hash %}
+{% if vars.MAIN_CORE_RECEIPTS_BY_HASH_ENABLED %}
 QUALIFY ROW_NUMBER() OVER (PARTITION BY tx_hash ORDER BY block_number DESC, _inserted_timestamp DESC) = 1
 {% else %}
 QUALIFY(ROW_NUMBER() OVER (PARTITION BY block_number, array_index ORDER BY _inserted_timestamp DESC)) = 1
