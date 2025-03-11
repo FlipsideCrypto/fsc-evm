70b1cfd154a41c8a824b054ebf22dc0ef544c9e0
Austin <austin@flipsidecrypto.com>
2025-03-11 16:17:07 -0400

    macro

--- macros/utilities/variables/auto_sets/set_dynamic_fields.sql
+++ macros/utilities/variables/auto_sets/set_dynamic_fields.sql
@@ -32,7 +32,7 @@
     {% set rpc_settings_query %}
     select 
         {% for item in fields_to_check %}
-            array_contains('{{ item.field }}'::VARIANT, {{ item.source }}) as {{ item.field }},
+            array_contains('{{ item.field }}'::VARIANT, {{ item.source }}) as "{{ item.field }}",
         {% endfor %}
         1 as dummy  -- Prevents trailing comma issue
     from {{ ref('rpc__node_responses') }}
@@ -42,23 +42,17 @@
 
     {% if execute %}
         {% set row = results.rows[0] %}
-        {% set return_dict = {} %}
+        
+        {# Create dictionary to store field values #}
+        {% set field_dict = {} %}
         
         {% for item in fields_to_check %}
-            {# Create a dictionary-like object that supports both dot and bracket notation #}
-            {% do return_dict.update({
-                item.field: row[item.field]
-            }) %}
+            {# Use quotes to preserve case sensitivity in column names #}
+            {% set _ = field_dict.update({item.field: row[item.field]}) %}
         {% endfor %}
         
-        {# Make the dictionary support attribute access (dot notation) #}
-        {% set attr_dict = namespace() %}
-        {% for key, value in return_dict.items() %}
-            {% do attr_dict.__setattr__(key, value) %}
-        {% endfor %}
-        
-        {% do return(attr_dict) %}
+        {% do return(field_dict) %}
     {% else %}
-        {% do return(namespace()) %}
+        {% do return({}) %}
     {% endif %}
 {% endmacro %}
