84d7f3014bf14e5d09f6176d5f34312e351c577d
sam <sam@flipsidecrypto.com>
2025-03-12 21:23:59 +0800

    update silververified abi test

--- models/decoder_package/abis/silver/tests/verified_abis/test_silver__verified_abis_recent.sql
+++ models/decoder_package/abis/silver/tests/verified_abis/test_silver__verified_abis_recent.sql
@@ -1,6 +1,8 @@
 {# Log configuration details #}
 {{ log_model_details() }}
 
+{% set decoder_abis_block_explorer_name = var('DECODER_ABIS_BLOCK_EXPLORER_NAME', '') %}
+
 {{ config (
     materialized = "view",
     tags = ['daily_test']
@@ -12,3 +14,4 @@ FROM
     {{ ref('silver__verified_abis') }}
 WHERE
     _inserted_timestamp > DATEADD(DAY, -5, SYSDATE())
+    and abi_source = LOWER('{{ decoder_abis_block_explorer_name }}') 
