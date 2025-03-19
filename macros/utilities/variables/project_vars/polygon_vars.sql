cde26a995ef81287ee4c6a1d9c73a046dd181403
drethereum <trevor.wenokur@gmail.com>
2025-03-19 15:13:42 -0600

    typo

--- macros/utilities/variables/project_vars/polygon_vars.sql
+++ macros/utilities/variables/project_vars/polygon_vars.sql
@@ -4,7 +4,7 @@
         'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/polygon/quicknode/mainnet',
         'GLOBAL_WRAPPED_NATIVE_ASSET_ADDRESS': '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
         'MAIN_SL_BLOCKS_PER_HOUR': 1700,
-        'MAIN_PRICES_NATIVE_SYMBOLS': ['MATIC','POL']
+        'MAIN_PRICES_NATIVE_SYMBOLS': ['MATIC','POL'],
         'MAIN_PRICES_PROVIDER_PLATFORMS': 'Polygon',
         'DECODER_SILVER_CONTRACT_ABIS_EXPLORER_NAME': 'polygonscan',
         'DECODER_SL_CONTRACT_ABIS_EXPLORER_URL': 'https://api.polygonscan.com/api?module=contract&action=getabi&address=',
