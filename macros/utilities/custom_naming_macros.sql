cfe6d78cc4171d6dc83be8d79fb919291f32a4bf
drethereum <trevor.wenokur@gmail.com>
2025-03-31 12:59:53 -0600

    test schema

--- macros/utilities/custom_naming_macros.sql
+++ macros/utilities/custom_naming_macros.sql
@@ -1,7 +1,13 @@
 {% macro generate_schema_name(custom_schema_name=none, node=none) -%}
+    {% set vars = return_vars() %}
     {% set node_name = node.name %}
     {% set split_name = node_name.split('__') %}
-    {{ split_name[0] | trim }}
+    
+    {% if split_name[0] == 'core' and vars.GOLD_CORE_SCHEMA_NAME is not none %}
+        {{ vars.GOLD_CORE_SCHEMA_NAME | trim }}
+    {% else %}
+        {{ split_name[0] | trim }}
+    {% endif %}
 {%- endmacro %}
 
 {% macro generate_alias_name(custom_alias_name=none, node=none) -%}
