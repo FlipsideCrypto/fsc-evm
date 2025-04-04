4184450234cce97d1804354a2ef818184d151460
drethereum <trevor.wenokur@gmail.com>
2025-04-04 13:49:29 -0600

    remove var

--- macros/global/variables/project_vars/fsc_evm_vars.sql
+++ macros/global/variables/project_vars/fsc_evm_vars.sql
@@ -1,7 +1,6 @@
 {% macro fsc_evm_vars() %}
     {% set vars = {
-        'GLOBAL_PROJECT_NAME': 'fsc_evm',
-        'MAIN_GHA_CHAINHEAD_SCHEDULE': '1,31 * * * *'
+        'GLOBAL_PROJECT_NAME': 'fsc_evm'
     } %}
     
     {{ return(vars) }}
