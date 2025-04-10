c1c3823f0c6570a98d583e7f40a9a6d329d8c578
drethereum <trevor.wenokur@gmail.com>
2025-04-10 13:24:09 -0600

    updated for dev

--- macros/global/livequery/livequery_grants.sql
+++ macros/global/livequery/livequery_grants.sql
@@ -1,53 +1,55 @@
 {% macro livequery_grants() %}
 
-{% set prod_db_name = (target.database | replace('_dev', '') | upper) %}
+{% set vars = return_vars() %}
+{% set target_db = target.database | upper %}
+{% set project = vars.GLOBAL_PROJECT_NAME | upper %}
 
-    {% if var("UPDATE_UDFS_AND_SPS", false) and target.database | upper == prod_db_name and target.name == 'prod' %}
-    
-        {% do run_query("GRANT USAGE ON SCHEMA " ~ prod_db_name ~ "._live TO AWS_LAMBDA_" ~ prod_db_name ~ "_API;") %}
-        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ prod_db_name ~ "._live TO AWS_LAMBDA_" ~ prod_db_name ~ "_API;") %}
-        {{ log("Permissions granted to role AWS_LAMBDA_" ~ prod_db_name ~ "_API for schema " ~ prod_db_name ~ "._live", info=True) }}
+    {% if var("UPDATE_UDFS_AND_SPS", false) %}
 
-        {% do run_query("GRANT USAGE ON SCHEMA " ~ prod_db_name ~ "._live TO DBT_CLOUD_" ~ prod_db_name ~ ";") %}
-        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ prod_db_name ~ "._live TO DBT_CLOUD_" ~ prod_db_name ~ ";") %}
-        {{ log("Permissions granted to role DBT_CLOUD_" ~ prod_db_name ~ " for schema " ~ prod_db_name ~ "._live", info=True) }}
+        {% do run_query("GRANT USAGE ON SCHEMA " ~ target_db ~ "._live TO AWS_LAMBDA_" ~ project ~ "_API;") %}
+        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ target_db ~ "._live TO AWS_LAMBDA_" ~ project ~ "_API;") %}
+        {{ log("Permissions granted to role AWS_LAMBDA_" ~ project ~ "_API for schema " ~ target_db ~ "._live", info=True) }}
 
-        {% do run_query("GRANT USAGE ON SCHEMA " ~ prod_db_name ~ "._live TO INTERNAL_DEV;") %}
-        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ prod_db_name ~ "._live TO INTERNAL_DEV;") %}
-        {{ log("Permissions granted to role INTERNAL_DEV for schema " ~ prod_db_name ~ "._live", info=True) }}
+        {% do run_query("GRANT USAGE ON SCHEMA " ~ target_db ~ "._live TO DBT_CLOUD_" ~ project ~ ";") %}
+        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ target_db ~ "._live TO DBT_CLOUD_" ~ project ~ ";") %}
+        {{ log("Permissions granted to role DBT_CLOUD_" ~ project ~ " for schema " ~ target_db ~ "._live", info=True) }}
 
-        {% do run_query("GRANT USAGE ON SCHEMA " ~ prod_db_name ~ "._utils TO AWS_LAMBDA_" ~ prod_db_name ~ "_API;") %}
-        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ prod_db_name ~ "._utils TO AWS_LAMBDA_" ~ prod_db_name ~ "_API;") %}
-        {{ log("Permissions granted to role AWS_LAMBDA_" ~ prod_db_name ~ "_API for schema " ~ prod_db_name ~ "._utils", info=True) }}
+        {% do run_query("GRANT USAGE ON SCHEMA " ~ target_db ~ "._live TO INTERNAL_DEV;") %}
+        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ target_db ~ "._live TO INTERNAL_DEV;") %}
+        {{ log("Permissions granted to role INTERNAL_DEV for schema " ~ target_db ~ "._live", info=True) }}
 
-        {% do run_query("GRANT USAGE ON SCHEMA " ~ prod_db_name ~ "._utils TO DBT_CLOUD_" ~ prod_db_name ~ ";") %}
-        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ prod_db_name ~ "._utils TO DBT_CLOUD_" ~ prod_db_name ~ ";") %}
-        {{ log("Permissions granted to role DBT_CLOUD_" ~ prod_db_name ~ " for schema " ~ prod_db_name ~ "._utils", info=True) }}
+        {% do run_query("GRANT USAGE ON SCHEMA " ~ target_db ~ "._utils TO AWS_LAMBDA_" ~ project ~ "_API;") %}
+        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ target_db ~ "._utils TO AWS_LAMBDA_" ~ project ~ "_API;") %}
+        {{ log("Permissions granted to role AWS_LAMBDA_" ~ project ~ "_API for schema " ~ target_db ~ "._utils", info=True) }}
 
-        {% do run_query("GRANT USAGE ON SCHEMA " ~ prod_db_name ~ "._utils TO INTERNAL_DEV;") %}
-        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ prod_db_name ~ "._utils TO INTERNAL_DEV;") %}
-        {{ log("Permissions granted to role INTERNAL_DEV for schema " ~ prod_db_name ~ "._utils", info=True) }}
+        {% do run_query("GRANT USAGE ON SCHEMA " ~ target_db ~ "._utils TO DBT_CLOUD_" ~ project ~ ";") %}
+        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ target_db ~ "._utils TO DBT_CLOUD_" ~ project ~ ";") %}
+        {{ log("Permissions granted to role DBT_CLOUD_" ~ project ~ " for schema " ~ target_db ~ "._utils", info=True) }}
+
+        {% do run_query("GRANT USAGE ON SCHEMA " ~ target_db ~ "._utils TO INTERNAL_DEV;") %}
+        {% do run_query("GRANT USAGE ON ALL FUNCTIONS IN SCHEMA " ~ target_db ~ "._utils TO INTERNAL_DEV;") %}
+        {{ log("Permissions granted to role INTERNAL_DEV for schema " ~ target_db ~ "._utils", info=True) }}
         
     {% else %}
-        {{ log("Error: Permission grants unsuccessful. Check if target is prod.", info=True) }}
+        {{ log("Error: Permission grants unsuccessful.", info=True) }}
     {% endif %}
 
 {% endmacro %}
 
 {% macro drop_livequery_schemas() %}
 
-{% if var("UPDATE_UDFS_AND_SPS", false) and target.database | upper == prod_db_name and target.name == 'prod' %}
+{% set target_db = target.database | upper %}
 
-    {% set drop_schemas_sql %}
-        DROP SCHEMA IF EXISTS _LIVE;
-        DROP SCHEMA IF EXISTS _UTILS;
-        DROP SCHEMA IF EXISTS LIVE;
-        DROP SCHEMA IF EXISTS UTILS;
-    {% endset %}
-    {% do run_query(drop_schemas_sql) %}
+{% if var("UPDATE_UDFS_AND_SPS", false) %}
+
+    {% do run_query("DROP SCHEMA IF EXISTS " ~ target_db ~ "._LIVE;") %}
+    {% do run_query("DROP SCHEMA IF EXISTS " ~ target_db ~ "._UTILS;") %}
+    {% do run_query("DROP SCHEMA IF EXISTS " ~ target_db ~ ".LIVE;") %}
+    {% do run_query("DROP SCHEMA IF EXISTS " ~ target_db ~ ".UTILS;") %}
+    {{ log("Schemas dropped successfully.", info=True) }}
 
 {% else %}
-    {{ log("Error: DROP SCHEMA unsuccessful. Check if target is prod.", info=True) }}
+    {{ log("Error: DROP SCHEMA unsuccessful.", info=True) }}
 {% endif %}
 
 {% endmacro %}
