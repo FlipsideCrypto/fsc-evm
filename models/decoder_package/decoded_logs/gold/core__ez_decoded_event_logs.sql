49b58eadc2909c408af584a37ab71089144e65c7
drethereum <trevor.wenokur@gmail.com>
2025-04-10 16:39:56 -0600

    formatting

--- models/decoder_package/decoded_logs/gold/core__ez_decoded_event_logs.sql
+++ models/decoder_package/decoded_logs/gold/core__ez_decoded_event_logs.sql
@@ -23,33 +23,29 @@ WITH base AS (
         event_index,
         event_name,
         contract_address,
-        decoded_data as full_decoded_log,
-        decoded_flat as decoded_log
+        decoded_data AS full_decoded_log,
+        decoded_flat AS decoded_log
     FROM
         {{ ref('silver__decoded_logs') }}
     WHERE
-        1=1
+        1 = 1
 
-    {% if is_incremental() %}
-    AND modified_timestamp > (
-        SELECT
-            COALESCE(
-                MAX(modified_timestamp),
-                '2000-01-01'::TIMESTAMP
-            )
-        FROM
-            {{ this }}
-    )
+{% if is_incremental() %}
+AND modified_timestamp > (
+    SELECT
+        COALESCE(MAX(modified_timestamp), '2000-01-01' :: TIMESTAMP)
+    FROM
+        {{ this }})
     {% endif %}
 ),
-new_records as (
+new_records AS (
     SELECT
-        b.block_number as block_number,
-        block_timestamp as block_timestamp,
-        b.tx_hash as tx_hash,
+        b.block_number AS block_number,
+        block_timestamp AS block_timestamp,
+        b.tx_hash AS tx_hash,
         tx_position,
-        b.event_index as event_index,
-        b.contract_address as contract_address,
+        b.event_index AS event_index,
+        b.contract_address AS contract_address,
         topics,
         topic_0,
         topic_1,
@@ -61,20 +57,28 @@ new_records as (
         origin_to_address,
         origin_function_signature,
         tx_succeeded,
-        event_name, 
+        event_name,
         full_decoded_log,
         decoded_log,
-        name as contract_name
-    FROM base b 
-    LEFT JOIN {{ ref('core__fact_event_logs') }} fel
-    on b.block_number = fel.block_number and b.event_index = fel.event_index
-    {% if is_incremental() %}
-        and fel.inserted_timestamp > dateadd('day', -3, sysdate())
-    {% endif %}
-    LEFT JOIN {{ ref('core__dim_contracts') }} dc
-    ON b.contract_address = dc.address and dc.name IS NOT NULL
-    WHERE 1=1
+        NAME AS contract_name
+    FROM
+        base b
+        LEFT JOIN {{ ref('core__fact_event_logs') }}
+        fel
+        ON b.block_number = fel.block_number
+        AND b.event_index = fel.event_index
+
+{% if is_incremental() %}
+AND fel.inserted_timestamp > DATEADD('day', -3, SYSDATE())
+{% endif %}
+LEFT JOIN {{ ref('core__dim_contracts') }}
+dc
+ON b.contract_address = dc.address
+AND dc.name IS NOT NULL
+WHERE
+    1 = 1
 )
+
 {% if is_incremental() %},
 missing_tx_data AS (
     SELECT
@@ -89,20 +93,28 @@ missing_tx_data AS (
         fel.topic_1,
         fel.topic_2,
         fel.topic_3,
-        fel.DATA,
+        fel.data,
         fel.event_removed,
         fel.origin_from_address,
         fel.origin_to_address,
         fel.origin_function_signature,
         fel.tx_succeeded,
-        t.event_name, 
+        t.event_name,
         t.full_decoded_log,
         t.decoded_log,
         t.contract_name
-    FROM {{ this }} t
-    INNER JOIN {{ ref('core__fact_event_logs') }} fel 
-    USING (block_number, event_index)
-    WHERE t.tx_succeeded IS NULL OR t.block_timestamp IS NULL and fel.block_timestamp IS NOT NULL
+    FROM
+        {{ this }}
+        t
+        INNER JOIN {{ ref('core__fact_event_logs') }}
+        fel USING (
+            block_number,
+            event_index
+        )
+    WHERE
+        t.tx_succeeded IS NULL
+        OR t.block_timestamp IS NULL
+        AND fel.block_timestamp IS NOT NULL
 ),
 missing_contract_data AS (
     SELECT
@@ -123,18 +135,22 @@ missing_contract_data AS (
         origin_to_address,
         origin_function_signature,
         tx_succeeded,
-        event_name, 
+        event_name,
         full_decoded_log,
         decoded_log,
-        dc.name as contract_name
-    FROM {{ this }} t
-    INNER JOIN {{ ref('core__dim_contracts') }} dc
-    ON t.contract_address = dc.address and dc.name IS NOT NULL
-    WHERE t.contract_name IS NULL
+        dc.name AS contract_name
+    FROM
+        {{ this }}
+        t
+        INNER JOIN {{ ref('core__dim_contracts') }}
+        dc
+        ON t.contract_address = dc.address
+        AND dc.name IS NOT NULL
+    WHERE
+        t.contract_name IS NULL
 )
-{% endif %}
-, 
-FINAL as (
+{% endif %},
+FINAL AS (
     SELECT
         block_number,
         block_timestamp,
@@ -153,67 +169,16 @@ FINAL as (
         origin_to_address,
         origin_function_signature,
         tx_succeeded,
-        event_name, 
+        event_name,
         full_decoded_log,
         decoded_log,
         contract_name
     FROM
         new_records
 
-    {% if is_incremental() %}
-    UNION ALL 
-    SELECT
-        block_number,
-        block_timestamp,
-        tx_hash,
-        tx_position,
-        event_index,
-        contract_address,
-        topics,
-        topic_0,
-        topic_1,
-        topic_2,
-        topic_3,
-        DATA,
-        event_removed,
-        origin_from_address,
-        origin_to_address,
-        origin_function_signature,
-        tx_succeeded,
-        event_name, 
-        full_decoded_log,
-        decoded_log,
-        contract_name
-    FROM
-        missing_tx_data
-    UNION ALL 
-    SELECT
-        block_number,
-        block_timestamp,
-        tx_hash,
-        tx_position,
-        event_index,
-        contract_address,
-        topics,
-        topic_0,
-        topic_1,
-        topic_2,
-        topic_3,
-        DATA,
-        event_removed,
-        origin_from_address,
-        origin_to_address,
-        origin_function_signature,
-        tx_succeeded,
-        event_name, 
-        full_decoded_log,
-        decoded_log,
-        contract_name
-    FROM
-        missing_contract_data
-    {% endif %}
-)
-SELECT 
+{% if is_incremental() %}
+UNION ALL
+SELECT
     block_number,
     block_timestamp,
     tx_hash,
@@ -231,21 +196,71 @@ SELECT
     origin_to_address,
     origin_function_signature,
     tx_succeeded,
-    event_name, 
+    event_name,
+    full_decoded_log,
+    decoded_log,
+    contract_name
+FROM
+    missing_tx_data
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    tx_position,
+    event_index,
+    contract_address,
+    topics,
+    topic_0,
+    topic_1,
+    topic_2,
+    topic_3,
+    DATA,
+    event_removed,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    tx_succeeded,
+    event_name,
+    full_decoded_log,
+    decoded_log,
+    contract_name
+FROM
+    missing_contract_data
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    tx_position,
+    event_index,
+    contract_address,
+    topics,
+    topic_0,
+    topic_1,
+    topic_2,
+    topic_3,
+    DATA,
+    event_removed,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    tx_succeeded,
+    event_name,
     full_decoded_log,
     decoded_log,
     contract_name,
     {{ dbt_utils.generate_surrogate_key(
-            ['tx_hash', 'event_index']
+        ['tx_hash', 'event_index']
     ) }} AS ez_decoded_event_logs_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp
-FROM FINAL
-qualify ROW_NUMBER() over (
-    PARTITION BY
-        ez_decoded_event_logs_id
-    ORDER BY
-        block_timestamp DESC nulls last,
-        tx_succeeded DESC nulls last,
-        contract_name DESC nulls last
-) = 1
+FROM
+    FINAL qualify ROW_NUMBER() over (
+        PARTITION BY ez_decoded_event_logs_id
+        ORDER BY
+            block_timestamp DESC nulls last,
+            tx_succeeded DESC nulls last,
+            contract_name DESC nulls last
+    ) = 1
