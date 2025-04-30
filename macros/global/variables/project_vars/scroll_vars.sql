c09032a48dab42f5c24deae985f0d14add4a1710
drethereum <trevor.wenokur@gmail.com>
2025-04-30 14:17:17 -0600

    cron

--- macros/global/variables/project_vars/scroll_vars.sql
+++ macros/global/variables/project_vars/scroll_vars.sql
@@ -5,6 +5,7 @@
         'GLOBAL_NODE_URL': '{URL}',
         'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/evm/quicknode/scroll/mainnet',
         'GLOBAL_WRAPPED_NATIVE_ASSET_ADDRESS': '0x5300000000000000000000000000000000000004',
+        'MAIN_GHA_STREAMLINE_CHAINHEAD_CRON': '12,42 * * * *',
         'MAIN_SL_BLOCKS_PER_HOUR': 1200,
         'MAIN_PRICES_NATIVE_SYMBOLS': ['ETH', 'SCROLL'],
         'MAIN_PRICES_NATIVE_BLOCKCHAINS': ['ethereum', 'scroll'],
