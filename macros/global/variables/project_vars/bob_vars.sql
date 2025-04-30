6c9b75a5bc2c4aa7b78d80d0fb63bb339eb13bdd
drethereum <trevor.wenokur@gmail.com>
2025-04-30 16:23:58 -0600

    vars/bob-boba

--- macros/global/variables/project_vars/bob_vars.sql
+++ macros/global/variables/project_vars/bob_vars.sql
@@ -11,7 +11,8 @@
         'MAIN_PRICES_PROVIDER_PLATFORMS': 'bob-network',
         'DECODER_SILVER_CONTRACT_ABIS_EXPLORER_NAME': 'GoBOB',
         'DECODER_SL_CONTRACT_ABIS_EXPLORER_URL': 'https://explorer-bob-mainnet-0.t.conduit.xyz/api/v2/smart-contracts/',
-        'DECODER_SL_CONTRACT_ABIS_BRONZE_TABLE_ENABLED': true
+        'DECODER_SL_CONTRACT_ABIS_BRONZE_TABLE_ENABLED': true,
+        'MAIN_GHA_STREAMLINE_CHAINHEAD_CRON': '21,51 * * * *'
     } %}
     
     {{ return(vars) }}
--- macros/global/variables/project_vars/boba_vars.sql
+++ macros/global/variables/project_vars/boba_vars.sql
@@ -12,7 +12,8 @@
         'DECODER_SILVER_CONTRACT_ABIS_EXPLORER_NAME': 'routescan',
         'DECODER_SILVER_CONTRACT_ABIS_ETHERSCAN_ENABLED': true,
         'DECODER_SL_CONTRACT_ABIS_EXPLORER_URL': 'https://api.routescan.io/v2/network/mainnet/evm/288/etherscan/api?module=contract&action=getabi&address=',
-        'DECODER_SL_CONTRACT_ABIS_BRONZE_TABLE_ENABLED': true
+        'DECODER_SL_CONTRACT_ABIS_BRONZE_TABLE_ENABLED': true,
+        'MAIN_GHA_STREAMLINE_CHAINHEAD_CRON': '3,33 * * * *'
     } %}
     
     {{ return(vars) }}
