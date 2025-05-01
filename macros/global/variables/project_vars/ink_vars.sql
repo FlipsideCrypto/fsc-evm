cb22aff16dd23535cf19a5d78726125641abe1b0
drethereum <trevor.wenokur@gmail.com>
2025-05-01 09:57:32 -0600

    url

--- macros/global/variables/project_vars/ink_vars.sql
+++ macros/global/variables/project_vars/ink_vars.sql
@@ -2,7 +2,8 @@
     {% set vars = {
         'GLOBAL_PROJECT_NAME': 'ink',
         'GLOBAL_NODE_PROVIDER': 'quicknode',
-        'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/ink/quicknode/mainnet',
+        'GLOBAL_NODE_URL': "{URL}",
+        'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/evm/ink/quicknode/mainnet',
         'GLOBAL_WRAPPED_NATIVE_ASSET_ADDRESS': '0x4200000000000000000000000000000000000006',
         'MAIN_SL_BLOCKS_PER_HOUR': 3600,
         'MAIN_PRICES_NATIVE_SYMBOLS': 'ETH',
