732fdd9ebc685ac782b3a41473389a3acd661e00
drethereum <trevor.wenokur@gmail.com>
2025-05-05 12:39:46 -0600

    0

--- models/main_package/core/gold/core__fact_transactions.sql
+++ models/main_package/core/gold/core__fact_transactions.sql
@@ -181,7 +181,7 @@ WHERE
             utils.udf_hex_to_int(r.receipts_json :blobGasUsed :: STRING) as blob_gas_used,
             {% endif %}
             {% if rpc_vars.l1Fee %}
-            COALESCE(utils.udf_hex_to_int(r.receipts_json :l1Fee :: STRING),0 :: STRING) as l1_fee_precise_raw,
+            utils.udf_hex_to_int(r.receipts_json :l1Fee :: STRING) as l1_fee_precise_raw,
             utils.udf_decimal_adjust(l1_fee_precise_raw, 18) as l1_fee_precise,
             l1_fee_precise :: FLOAT AS l1_fee,
             {% endif %}
@@ -390,7 +390,7 @@ missing_data AS (
         utils.udf_hex_to_int(r.receipts_json :blobGasUsed :: STRING) as blob_gas_used_heal,
         {% endif %}
         {% if rpc_vars.l1Fee %}
-        COALESCE(utils.udf_hex_to_int(r.receipts_json :l1Fee :: STRING),0 :: STRING) as l1_fee_precise_raw_heal,
+        utils.udf_hex_to_int(r.receipts_json :l1Fee :: STRING) as l1_fee_precise_raw_heal,
         l1_fee_precise_raw_heal :: FLOAT AS l1_fee_heal,
         utils.udf_decimal_adjust(l1_fee_precise_raw_heal, 18) as l1_fee_precise_heal,
         {% endif %}
@@ -754,7 +754,7 @@ SELECT
     value_precise_raw,
     value_precise,
     tx_fee,
-    tx_fee_precise,
+    COALESCE(tx_fee_precise,'0') AS tx_fee_precise,
     tx_succeeded,
     tx_type,
     nonce,
