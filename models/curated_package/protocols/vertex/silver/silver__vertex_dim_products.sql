f80b7205121da250a9ba851536ef401ed1d0caf4
mattromano <mattromano88@gmail.com>
2025-05-14 10:44:12 -0700

    test var

--- models/curated_package/protocols/vertex/silver/silver__vertex_dim_products.sql
+++ models/curated_package/protocols/vertex/silver/silver__vertex_dim_products.sql
@@ -2,7 +2,6 @@
 {% set vars = return_vars() %}
 
 {# Override project name for API endpoint #}
-{% set project_name = var('CURATED_VERTEX_PROJECT_NAME', vars.GLOBAL_PROJECT_NAME) %}
 
 {# Log configuration details #}
 {{ log_model_details() }}
@@ -64,7 +63,7 @@ api_pull AS (
     SELECT
         PARSE_JSON(
             live.udf_api(
-                'https://gateway.' || '{{ project_name }}' || '-prod.vertexprotocol.com/api/v2/assets'
+                'https://gateway.' || '{{ vars.CURATED_VERTEX_PROJECT_NAME }}' || '-prod.vertexprotocol.com/api/v2/assets'
 
             )
         ) :data AS response
