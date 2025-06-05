b0ab3b6da4d90e9c99ce1db61cf8256e56d10666
drethereum <trevor.wenokur@gmail.com>
2025-06-05 15:41:17 -0600

    blocks per hour

--- macros/global/variables/project_vars/mezo_vars.sql
+++ macros/global/variables/project_vars/mezo_vars.sql
@@ -3,7 +3,7 @@
         'GLOBAL_PROJECT_NAME': 'mezo',
         'GLOBAL_NODE_PROVIDER': 'imperator',
         'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/evm/imperator/mezo/mainnet',
-        'MAIN_SL_BLOCKS_PER_HOUR': 2000,
+        'MAIN_SL_BLOCKS_PER_HOUR': 1000,
         'MAIN_GHA_STREAMLINE_CHAINHEAD_CRON': '22,52 * * * *',
         'MAIN_GHA_SCHEDULED_SCORES_CRON': '5 5 * * *'
     } %}
