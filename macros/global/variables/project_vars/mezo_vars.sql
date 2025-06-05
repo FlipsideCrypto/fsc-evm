0e6505e55a32e12e4a65d04a59e1c1ec665bbff1
drethereum <trevor.wenokur@gmail.com>
2025-06-05 14:54:48 -0600

    provider

--- macros/global/variables/project_vars/mezo_vars.sql
+++ macros/global/variables/project_vars/mezo_vars.sql
@@ -1,7 +1,7 @@
 {% macro mezo_vars() %}
     {% set vars = {
         'GLOBAL_PROJECT_NAME': 'mezo',
-        'GLOBAL_NODE_PROVIDER': 'quicknode',
+        'GLOBAL_NODE_PROVIDER': 'imperator',
         'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/evm/imperator/mezo/mainnet',
         'MAIN_SL_BLOCKS_PER_HOUR': 2000,
         'MAIN_GHA_STREAMLINE_CHAINHEAD_CRON': '22,52 * * * *',
