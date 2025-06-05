3a08882509b06df1583c7a41a3d683ab94f83763
drethereum <trevor.wenokur@gmail.com>
2025-06-05 16:10:15 -0600

    receipts by hash var

--- macros/global/variables/project_vars/mezo_vars.sql
+++ macros/global/variables/project_vars/mezo_vars.sql
@@ -4,6 +4,7 @@
         'GLOBAL_NODE_PROVIDER': 'imperator',
         'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/evm/imperator/mezo/mainnet',
         'MAIN_SL_BLOCKS_PER_HOUR': 1000,
+        'MAIN_CORE_RECEIPTS_BY_HASH_ENABLED': true,
         'MAIN_GHA_STREAMLINE_CHAINHEAD_CRON': '22,52 * * * *',
         'MAIN_GHA_SCHEDULED_SCORES_CRON': '5 5 * * *'
     } %}
