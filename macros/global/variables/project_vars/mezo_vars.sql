40bd713617e797f04fed08dea67a2f6c117736fe
drethereum <trevor.wenokur@gmail.com>
2025-06-06 11:27:29 -0600

    abi url

--- macros/global/variables/project_vars/mezo_vars.sql
+++ macros/global/variables/project_vars/mezo_vars.sql
@@ -6,6 +6,11 @@
         'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/evm/imperator/mezo/mainnet',
         'MAIN_SL_BLOCKS_PER_HOUR': 1000,
         'MAIN_CORE_RECEIPTS_BY_HASH_ENABLED': true,
+        'MAIN_PRICES_NATIVE_SYMBOLS': 'BTC',
+        'MAIN_PRICES_NATIVE_BLOCKCHAINS': 'bitcoin',
+        'MAIN_PRICES_PROVIDER_PLATFORMS': 'Mezo',
+        'DECODER_SILVER_CONTRACT_ABIS_EXPLORER_NAME': 'MezoExplorer',
+        'DECODER_SL_CONTRACT_ABIS_EXPLORER_URL': 'https://api.explorer.mezo.org/api/v2/smart-contracts/',
         'MAIN_GHA_STREAMLINE_CHAINHEAD_CRON': '22,52 * * * *',
         'MAIN_GHA_SCHEDULED_SCORES_CRON': '5 5 * * *'
     } %}
