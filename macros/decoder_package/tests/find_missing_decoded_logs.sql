d023d6698c08f1fa3c660ac6a9a7b3909b5e6dbc
Austin <austin@flipsidecrypto.com>
2025-06-26 13:24:23 -0400

    test update

--- macros/decoder_package/tests/find_missing_decoded_logs.sql
+++ macros/decoder_package/tests/find_missing_decoded_logs.sql
@@ -12,7 +12,8 @@ FROM
     d
     ON d.ez_decoded_event_logs_id = l.fact_event_logs_id
 WHERE
-    l.contract_address = '{{ vars.GLOBAL_WRAPPED_NATIVE_ASSET_ADDRESS }}'
+    l.tx_succeeded
+    AND l.contract_address = '{{ vars.GLOBAL_WRAPPED_NATIVE_ASSET_ADDRESS }}'
     AND l.topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- Transfer
     AND d.ez_decoded_event_logs_id IS NULL
     AND l.block_timestamp < (SELECT MAX(block_timestamp) - INTERVAL '2 hours' FROM {{ fact_logs_model }})
