ed911baa10b75914b48062e3bdaa566f4253e24f
Austin <austin@flipsidecrypto.com>
2025-07-01 15:31:54 -0400

    params

--- macros/global/variables/project_vars/bsc_vars.sql
+++ macros/global/variables/project_vars/bsc_vars.sql
@@ -5,7 +5,7 @@
         'GLOBAL_NODE_VAULT_PATH': 'Vault/prod/evm/quicknode/bsc/mainnet',
         'GLOBAL_NODE_URL': '{URL}',
         'GLOBAL_WRAPPED_NATIVE_ASSET_ADDRESS': '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
-        'MAIN_SL_BLOCKS_PER_HOUR': 1200,
+        'MAIN_SL_BLOCKS_PER_HOUR': 4800,
         'MAIN_PRICES_NATIVE_SYMBOLS': 'BNB',
         'MAIN_PRICES_PROVIDER_PLATFORMS': ['BNB','BNB Smart Chain (BEP20)'],
         'DECODER_SILVER_CONTRACT_ABIS_EXPLORER_NAME': 'bscscan',
