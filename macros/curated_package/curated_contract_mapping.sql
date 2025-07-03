a1340af73a488a7f11244d412e3aadde442414d5
drethereum <trevor.wenokur@gmail.com>
2025-07-03 11:55:27 -0600

    test vars

--- macros/curated_package/curated_contract_mapping.sql
+++ macros/curated_package/curated_contract_mapping.sql
@@ -1,8 +1,10 @@
 {% macro curated_contract_mapping(contract_mapping_dict) %}
     SELECT * FROM VALUES
-    {% for platform, contract_info in contract_mapping_dict.items() %}
-        ('{{ contract_info.contract_address }}', '{{ platform }}', '{{ contract_info.protocol }}', '{{ contract_info.version }}')
-        {%- if not loop.last -%},{%- endif %}
+    {% for protocol, contract_info in contract_mapping_dict.items() %}
+        {% for contract_address in contract_info.contract_address %}
+            ('{{ contract_address }}', '{{ protocol }}-{{ contract_info.version }}', '{{ protocol }}', '{{ contract_info.version }}')
+            {%- if not (loop.last and loop.outer.last) -%},{%- endif %}
+        {% endfor %}
     {% endfor %}
     AS t(contract_address, platform, protocol, version)
 {% endmacro %}
