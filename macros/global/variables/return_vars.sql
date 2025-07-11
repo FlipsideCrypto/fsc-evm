cab59098b6f2d9da645aca30c55c4a3377d208e1
Austin <austin@flipsidecrypto.com>
2025-07-11 15:01:55 -0400

    start block param

--- macros/global/variables/return_vars.sql
+++ macros/global/variables/return_vars.sql
@@ -16,6 +16,7 @@
   {% set ns.GLOBAL_MAX_SEQUENCE_NUMBER = get_var('GLOBAL_MAX_SEQUENCE_NUMBER', 1000000000) %}
   {% set ns.GLOBAL_NODE_VAULT_PATH = get_var('GLOBAL_NODE_VAULT_PATH', '') %}
   {% set ns.GLOBAL_NETWORK = get_var('GLOBAL_NETWORK', 'mainnet') %}
+  {% set ns.GLOBAL_START_BLOCK = get_var('GLOBAL_START_BLOCK', 1) %}
 
   {% set ns.GLOBAL_BRONZE_FR_ENABLED = none if get_var('GLOBAL_BRONZE_FR_ENABLED', false) else false %} {# Sets to none if true, still requires --full-refresh, otherwise will use incremental #}
   {% set ns.GLOBAL_SILVER_FR_ENABLED = none if get_var('GLOBAL_SILVER_FR_ENABLED', false) else false %} 
