580da6a01cfc9a4f2492f59058ceb550b20c3a7f
mattromano <mattromano88@gmail.com>
2025-07-21 11:09:43 -0700

    update vars, add compv2 token model

--- models/curated_package/defi/lending/silver/forks/compound_v2/silver__comp_v2_fork_asset_details.sql
+++ models/curated_package/defi/lending/silver/forks/compound_v2/silver__comp_v2_fork_asset_details.sql
@@ -1,15 +1,28 @@
+{# Get variables #}
+{% set vars = return_vars() %}
+
+{# Log configuration details #}
+{{ log_model_details() }}
+
 {{ config(
     materialized = 'incremental',
-    incremental_strategy = 'delete+insert',
-    unique_key = "block_number",
-    cluster_by = ['block_timestamp::DATE'],
+    unique_key = "atoken_address",
     tags = ['silver','defi','lending','curated']
 ) }}
 
-WITH contracts AS (
-
+WITH origin_from_addresses AS (
+    {{ curated_contract_mapping(
+        vars.CURATED_DEFI_LENDING_CONTRACT_MAPPING
+    ) }}
+    WHERE
+        type = 'comp_v2_origin_from_address'
+),
+contracts AS (
     SELECT
-        *
+        contract_address,
+        token_name,
+        token_symbol,
+        token_decimals
     FROM
         {{ ref('silver__contracts') }}
 ),
@@ -18,6 +31,7 @@ log_pull AS (
         l.tx_hash,
         l.block_number,
         l.block_timestamp,
+        l.origin_from_address,
         l.contract_address,
         C.token_name,
         C.token_symbol,
@@ -35,7 +49,12 @@ log_pull AS (
         ON C.contract_address = l.contract_address
     WHERE
         topics [0] :: STRING = '0x7ac369dbd14fa5ea3f473ed67cc9d598964a77501540ba6751eb0b3decf5870d'
-        AND C.token_name LIKE '%Comp V2 Fork%'
+        AND origin_from_address IN (
+            SELECT
+                origin_from_address
+            FROM
+                origin_from_addresses
+        )
 
 {% if is_incremental() %}
 AND l.modified_timestamp >= (
@@ -95,24 +114,93 @@ underlying_details AS (
         t.asset_identifier = 1 qualify(ROW_NUMBER() over(PARTITION BY l.contract_address
     ORDER BY
         block_timestamp ASC)) = 1
+),
+{% if is_incremental() %}
+contract_detail_heal AS (
+    SELECT
+        l.tx_hash,
+        l.block_number,
+        l.block_timestamp,
+        l.token_address,
+        c1.token_name,
+        c1.token_symbol,
+        c1.token_decimals,
+        underlying_asset_address,
+        c2.token_name AS underlying_name,
+        c2.token_symbol AS underlying_symbol,
+        c2.token_decimals AS underlying_decimals,
+        l._inserted_timestamp,
+        l._log_id
+    FROM
+        {{ this }} l
+    WHERE
+        (
+            l.token_name IS NULL
+            AND c1.token_name IS NOT NULL
+        )
+        OR (
+            l.underlying_name IS NULL
+            AND c2.token_name IS NOT NULL
+        )
+    LEFT JOIN contracts c1
+    ON c1.contract_address = l.underlying_asset
+    LEFT JOIN contracts c2
+    ON c2.contract_address = l.token_address
+),
+{% endif %}
+final AS (
+    SELECT
+        l.tx_hash,
+        l.block_number,
+        l.block_timestamp,
+        l.contract_address AS token_address,
+        l.token_name,
+        l.token_symbol,
+        l.token_decimals,
+        l.underlying_asset AS underlying_asset_address,
+        C.token_name AS underlying_name,
+        C.token_symbol AS underlying_symbol,
+        C.token_decimals AS underlying_decimals,
+        l._inserted_timestamp,
+        l._log_id
+    FROM
+        underlying_details l
+        LEFT JOIN contracts C
+        ON C.contract_address = l.underlying_asset
 )
+
 SELECT
-    l.tx_hash,
-    l.block_number,
-    l.block_timestamp,
-    l.contract_address AS token_address,
-    l.token_name,
-    l.token_symbol,
-    l.token_decimals,
-    l.underlying_asset AS underlying_asset_address,
-    C.token_name AS underlying_name,
-    C.token_symbol AS underlying_symbol,
-    C.token_decimals AS underlying_decimals,
-    l._inserted_timestamp,
-    l._log_id
+    tx_hash,
+    block_number,
+    block_timestamp,
+    token_address,
+    token_name,
+    token_symbol,
+    token_decimals,
+    underlying_asset_address,
+    underlying_name,
+    underlying_symbol,
+    underlying_decimals,
+    _inserted_timestamp,
+    _log_id
 FROM
-    underlying_details l
-    LEFT JOIN contracts C
-    ON C.contract_address = l.underlying_asset
-WHERE
-    l.token_name IS NOT NULL
+    final
+{% if is_incremental() %}
+UNION ALL
+SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
+    token_address,
+    token_name,
+    token_symbol,
+    token_decimals,
+    underlying_asset_address,
+    underlying_name,
+    underlying_symbol,
+    underlying_decimals,
+    _inserted_timestamp,
+    _log_id
+FROM
+    contract_detail_heal
+{% endif %}
