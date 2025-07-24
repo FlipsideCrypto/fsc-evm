ac43724a589d0c7e1ddf12b089b03f3cf7637b2e
mattromano <mattromano88@gmail.com>
2025-07-23 19:11:26 -0700

    add gold layer

---
+++ models/curated_package/defi/lending/gold/defi__ez_lending_liquidations.sql
@@ -0,0 +1,54 @@
+ {{ config(
+    materialized = 'view',
+    persist_docs ={ "relation": true,
+    "columns": true },
+    meta={
+        'database_tags':{
+            'table': {
+                'PURPOSE': 'LENDING, LIQUIDATIONS'
+            }
+        }
+    },
+    tags = ['gold','defi','lending','curated','ez']
+) }}
+
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    contract_address,
+    event_name,
+    event_index,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    platform,
+    liquidator,
+    borrower,
+    protocol_market,
+    collateral_token,
+    collateral_token_symbol,
+    liquidated_amount_unadj,
+    liquidated_amount,
+    liquidated_amount_usd,
+    debt_token,
+    debt_token_symbol,
+    repaid_amount_unadj,
+    repaid_amount,
+    repaid_amount_usd,
+    COALESCE (
+        complete_lending_liquidations_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['tx_hash', 'event_index']
+        ) }}
+    ) AS ez_lending_liquidations_id,
+    COALESCE(
+        inserted_timestamp,
+        '2000-01-01'
+    ) AS inserted_timestamp,
+    COALESCE(
+        modified_timestamp,
+        '2000-01-01'
+    ) AS modified_timestamp
+FROM 
+    {{ ref('silver__complete_lending_liquidations') }}
