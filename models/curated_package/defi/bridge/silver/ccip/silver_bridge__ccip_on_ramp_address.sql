d4497d02a98f7e3932dfddf7c0dbe6d2192c7750
sam <sam@flipsidecrypto.com>
2025-08-04 19:11:55 +0800

    add new table

---
+++ models/curated_package/defi/bridge/silver/ccip/silver_bridge__ccip_on_ramp_address.sql
@@ -0,0 +1,64 @@
+{# Get variables #}
+{% set vars = return_vars() %}
+{# Log configuration details #}
+{{ log_model_details() }}
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "block_number",
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['silver_bridge','defi','bridge','curated']
+) }}
+
+WITH contract_mapping AS (
+    {{ curated_contract_mapping(
+        vars.curated_defi_bridge_contract_mapping
+    ) }}
+    WHERE
+        protocol = 'chainlink_ccip'
+),
+on_ramp_set AS (
+    SELECT
+        block_timestamp,
+        tx_hash,
+        event_name,
+        TRY_TO_NUMBER(
+            decoded_log :destChainSelector :: STRING
+        ) AS destChainSelector,
+        chain_name,
+        decoded_log :onRamp :: STRING AS onRampAddress,
+        m.protocol,
+        m.version,
+        m.type,
+        CONCAT(
+            m.protocol,
+            '-',
+            m.version
+        ) AS platform,
+        modified_timestamp
+    FROM
+        {{ ref('core__ez_decoded_event_logs') }}
+        l
+        INNER JOIN contract_mapping m
+        ON l.contract_address = m.contract_address
+        INNER JOIN {{ ref('silver_bridge__ccip_chain_seed') }}
+        ON destChainSelector = chain_selector
+    WHERE
+        topic_0 = '0x1f7d0ec248b80e5c0dde0ee531c4fc8fdb6ce9a2b3d90f560c74acd6a7202f23' -- onrampset
+        AND tx_succeeded
+        AND event_removed = FALSE
+
+{% if is_incremental() %}
+AND modified_timestamp >= (
+    SELECT
+        MAX(modified_timestamp) - INTERVAL '{{ vars.CURATED_LOOKBACK_HOURS }}'
+    FROM
+        {{ this }}
+)
+AND modified_timestamp >= SYSDATE() - INTERVAL '{{ vars.CURATED_LOOKBACK_DAYS }}'
+{% endif %}
+)
+SELECT
+    *
+FROM
+    on_ramp_set
