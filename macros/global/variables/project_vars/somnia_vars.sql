85810e77aea0f33a4227410ce23b8707a00d831b
drethereum <trevor.wenokur@gmail.com>
2025-08-11 16:12:13 -0600

    somnia vars

---
+++ macros/global/variables/project_vars/somnia_vars.sql
@@ -0,0 +1,21 @@
+{% macro ink_vars() %}
+    {% set vars = {
+        'GLOBAL_PROJECT_NAME': 'somnia',
+        'GLOBAL_NODE_PROVIDER': '',
+        'GLOBAL_NODE_URL': "{URL}",
+        'GLOBAL_NODE_VAULT_PATH': '',
+        'GLOBAL_WRAPPED_NATIVE_ASSET_ADDRESS': '',
+        'GLOBAL_WRAPPED_NATIVE_ASSET_SYMBOL': '',
+        'GLOBAL_NATIVE_ASSET_SYMBOL': '',
+        'MAIN_SL_BLOCKS_PER_HOUR': 0,
+        'MAIN_PRICES_NATIVE_SYMBOLS': '',
+        'MAIN_PRICES_NATIVE_BLOCKCHAINS': '',
+        'MAIN_PRICES_PROVIDER_PLATFORMS': 'somnia',
+        'DECODER_SILVER_CONTRACT_ABIS_EXPLORER_NAME': 'somnia',
+        'DECODER_SL_CONTRACT_ABIS_EXPLORER_URL': '',
+        'MAIN_GHA_STREAMLINE_CHAINHEAD_CRON': '9,39 * * * *',
+        'MAIN_GHA_SCHEDULED_SCORES_CRON': '50 5 * * *'
+    } %}
+    
+    {{ return(vars) }}
+{% endmacro %} 
