be1519d03de02c71ff2b5fef61b7da13861af3f2
drethereum <trevor.wenokur@gmail.com>
2025-08-29 10:18:05 -0600

    gold

--- models/balances_package/erc20/gold/balances__fact_balances_native.sql
+++ models/balances_package/erc20/gold/balances__fact_balances_native.sql
@@ -1,39 +1,34 @@
 -- depends on: {{ ref('bronze__balances_erc20') }}
 {{ config(
     materialized = 'incremental',
-    unique_key = 'fact_balances_erc20_id',
+    unique_key = 'fact_balances_native_id',
     cluster_by = ['block_timestamp::date','_inserted_timestamp::date'],
     incremental_predicates = ["dynamic_range", "block_number"],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
     merge_exclude_columns = ["inserted_timestamp"],
-    tags = ['gold','balances','erc20','phase_4']
+    tags = ['gold','balances','native','phase_4']
 ) }}
 
 SELECT
     VALUE :"BLOCK_NUMBER" :: NUMBER AS block_number,
     VALUE :"BLOCK_TIMESTAMP" :: TIMESTAMP AS block_timestamp,
     VALUE :"ADDRESS" :: STRING AS address,
-    VALUE :"CONTRACT_ADDRESS" :: STRING AS contract_address,
     TRY_TO_NUMBER(
-        CASE
-            WHEN LENGTH(
-                DATA :result :: STRING
-            ) <= 4300
-            AND DATA :result IS NOT NULL THEN utils.udf_hex_to_int(LEFT(DATA :result :: STRING, 66))
-            ELSE NULL
-        END
+        utils.udf_hex_to_int(
+            DATA :result :: STRING
+        )
     ) AS balance,
     _inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(
-        ['block_number','address','contract_address']
-    ) }} AS fact_balances_erc20_id,
+        ['block_number','address']
+    ) }} AS fact_balances_native_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
 
 {% if is_incremental() %}
-{{ ref('bronze__balances_erc20') }}
+{{ ref('bronze__balances_native') }}
 WHERE
     _inserted_timestamp >= (
         SELECT
@@ -43,11 +38,11 @@ WHERE
     )
     AND DATA :result :: STRING <> '0x' --double check this
 {% else %}
-    {{ ref('bronze__balances_erc20_fr') }}
+    {{ ref('bronze__balances_native_fr') }}
 WHERE
     DATA :result :: STRING <> '0x'
 {% endif %}
 
-qualify(ROW_NUMBER() over (PARTITION BY fact_balances_erc20_id
+qualify(ROW_NUMBER() over (PARTITION BY fact_balances_native_id
 ORDER BY
     _inserted_timestamp DESC)) = 1
