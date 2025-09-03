79f4c84ed3467ec67ac1b6299e2b830cd02e9e7b
drethereum <trevor.wenokur@gmail.com>
2025-09-03 09:39:59 -0600

    tests

---
+++ models/balances_package/erc20/gold/tests/erc20/test_gold__fact_balances_erc20_full.sql
@@ -0,0 +1,24 @@
+{# Get variables #}
+{% set vars = return_vars() %}
+
+{# Log configuration details #}
+{{ log_model_details() }}
+
+{{ config (
+    materialized = "view",
+    tags = ['test_gold','balances','erc20','full_test','phase_4']
+) }}
+
+SELECT
+    *
+FROM
+    {{ ref('balances__fact_balances_erc20') }}
+{% if vars.MAIN_OBSERV_EXCLUSION_LIST_ENABLED %}
+WHERE
+    block_number NOT IN (
+        SELECT
+            block_number :: INT
+        FROM
+            observability.exclusion_list
+    )
+{% endif %}
