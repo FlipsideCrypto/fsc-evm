ede03e0341c4c0f72635bfdb6f96cd6736b2ce10
drethereum <trevor.wenokur@gmail.com>
2025-09-18 14:51:40 -0600

    v2 lp actions

---
+++ models/curated_package/defi/dex/silver/pool_swap_evt/silver_dex__paircreated_evt_v2_pool_actions.sql
@@ -0,0 +1,215 @@
+{# Get variables #}
+{% set vars = return_vars() %}
+
+{# Log configuration details #}
+{{ log_model_details() }}
+
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'block_number',
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['silver_dex','defi','dex','curated']
+) }}
+
+WITH evt AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        l.contract_address AS pool_address,
+        token0 AS token_0,
+        token1 AS token_1,
+        topic_0,
+        topic_1,
+        topic_2,
+        topic_3,
+        data,
+        regexp_substr_all(SUBSTR(data, 3, len(data)), '.{64}') AS segmented_data,
+        p.protocol,
+        p.version,
+        p.type,
+        CONCAT(
+            p.protocol,
+            '-',
+            p.version
+        ) AS platform,
+        CONCAT(
+            l.tx_hash :: STRING,
+            '-',
+            l.event_index :: STRING
+        ) AS _log_id,
+        l.modified_timestamp
+    FROM
+        {{ ref('core__fact_event_logs') }}
+        l
+        INNER JOIN {{ref('silver_dex__paircreated_evt_v2_pools')}} p
+        ON l.contract_address = p.pool_address
+    WHERE
+        topic_0 IN ('0x4c209b5fc8ad50758f13e2e1088ba56a560dff690a1c6fef26394f4c03821c4f' --mint
+        '0xdccd412f0b1252819cb1fd330b93224ca42612892bb3f4f789976e6d81936496' --burn
+        )
+        AND tx_succeeded
+
+{% if is_incremental() %}
+AND modified_timestamp >= (
+    SELECT
+        MAX(modified_timestamp) - INTERVAL '{{ vars.CURATED_LOOKBACK_HOURS }}'
+    FROM
+        {{ this }}
+)
+AND modified_timestamp >= SYSDATE() - INTERVAL '{{ vars.CURATED_LOOKBACK_DAYS }}'
+{% endif %}
+),
+mint AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        'Mint' AS event_name,
+        pool_address,
+        token_0,
+        token_1,
+        CONCAT('0x', SUBSTR(topic_1, 27, 40)) AS sender_address,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [0] :: STRING
+            )
+        ) AS amount_0,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [1] :: STRING
+            )
+        ) AS amount_1,
+        protocol,
+        version,
+        type,
+        platform,
+        _log_id,
+        modified_timestamp
+    FROM
+        evt
+    WHERE
+        topic_0 = '0x4c209b5fc8ad50758f13e2e1088ba56a560dff690a1c6fef26394f4c03821c4f' --mint
+),
+burn AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        'Burn' AS event_name,
+        pool_address,
+        token_0,
+        token_1,
+        CONCAT('0x', SUBSTR(topic_1, 27, 40)) AS sender_address,
+        CONCAT('0x', SUBSTR(topic_2, 27, 40)) AS to_address,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [0] :: STRING
+            )
+        ) AS amount_0,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [1] :: STRING
+            )
+        ) AS amount_1,
+        protocol,
+        version,
+        type,
+        platform,
+        _log_id,
+        modified_timestamp
+    FROM
+        evt
+    WHERE
+        topic_0 = '0xdccd412f0b1252819cb1fd330b93224ca42612892bb3f4f789976e6d81936496' --burn
+),
+all_actions AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        event_name,
+        pool_address,
+        token_0,
+        token_1,
+        sender_address,
+        pool_address AS to_address,
+        amount_0 AS amount_0_unadj,
+        amount_1 AS amount_1_unadj,
+        protocol,
+        version,
+        type,
+        platform,
+        _log_id,
+        modified_timestamp
+    FROM
+        mint
+    UNION ALL
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        event_name,
+        pool_address,
+        token_0,
+        token_1,
+        sender_address,
+        to_address,
+        amount_0 AS amount_0_unadj,
+        amount_1 AS amount_1_unadj,
+        protocol,
+        version,
+        type,
+        platform,
+        _log_id,
+        modified_timestamp
+    FROM
+        burn
+)
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_index,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    event_name,
+    pool_address,
+    token_0,
+    token_1,
+    sender_address,
+    to_address,
+    amount_0,
+    amount_1,
+    protocol,
+    version,
+    type,
+    platform,
+    _log_id,
+    modified_timestamp
+FROM
+    all_actions qualify(ROW_NUMBER() over (PARTITION BY _log_id
+ORDER BY
+    modified_timestamp DESC)) = 1
