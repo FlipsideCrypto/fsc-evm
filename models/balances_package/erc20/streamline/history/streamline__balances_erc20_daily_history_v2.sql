9df3cb84f71eb12d63f0d2e4e39bf764c258a644
Austin <austin@flipsidecrypto.com>
2025-09-22 17:02:21 -0400

    filters

--- models/balances_package/erc20/streamline/history/streamline__balances_erc20_daily_history_v2.sql
+++ models/balances_package/erc20/streamline/history/streamline__balances_erc20_daily_history_v2.sql
@@ -1,5 +1,7 @@
 -- depends_on: {{ ref('streamline__balances_erc20_daily_records') }}
 -- depends_on: {{ ref('streamline__balances_erc20_daily_complete') }}
+-- depends_on: {{ ref('_max_block_by_date') }}
+
 {# Get variables #}
 {% set vars = return_vars() %}
 
@@ -7,141 +9,156 @@
 {{ log_model_details() }}
 
 {# Set up dbt configuration #}
-{{ config (
+{{ config(
     materialized = "table",
-    tags = ['streamline','balances','history','erc20','phase_4']
+    tags = ['streamline', 'balances', 'history', 'erc20', 'phase_4']
 ) }}
 
 {% if execute %}
 
-{% set block_dates_query %}
-create or replace temporary table silver.erc20_balances_daily_block_dates__intermediate_tmp as
-select distinct block_date from {{ ref("streamline__balances_erc20_daily_records") }}
-{% endset %}
-{% do run_query(block_dates_query) %}
+    {% set block_dates_query %}
+        CREATE OR REPLACE TEMPORARY TABLE silver.erc20_balances_daily_block_dates__intermediate_tmp AS
+        SELECT DISTINCT block_date 
+        FROM {{ ref("streamline__balances_erc20_daily_records") }}
+    {% endset %}
+    {% do run_query(block_dates_query) %}
 
-{% set snapshot_date_query %}
+    {% set snapshot_date_query %}
+        SELECT MIN(block_date) AS snapshot_date 
+        FROM silver.erc20_balances_daily_block_dates__intermediate_tmp
+    {% endset %}
 
-select min(block_date) as snapshot_date from silver.erc20_balances_daily_block_dates__intermediate_tmp
-
-{% endset %}
-
-    {% set snapshot_date = run_query(snapshot_date_query) [0] [0] %}
+    {% set snapshot_date = run_query(snapshot_date_query)[0][0] %}
     {% if not snapshot_date or snapshot_date == 'None' %}
         {% set snapshot_date = '2099-01-01' %}
     {% endif %}
 
-{% set block_numbers_query %}
-create or replace temporary table silver.erc20_balances_daily_block_numbers__intermediate_tmp as
-select block_number, block_date
-from {{ ref("_max_block_by_date")}}
-where block_date in (select block_date from silver.erc20_balances_daily_block_dates__intermediate_tmp)
-{% endset %}
-{% do run_query(block_numbers_query) %}
+    {% set max_block_by_date_sql %}
+        WITH base AS (
+            SELECT
+                block_timestamp :: DATE AS block_date,
+                MAX(block_number) AS block_number
+            FROM
+                {{ ref("core__fact_blocks") }}
+            WHERE block_timestamp >= dateadd('day',-1,'{{ vars.BALANCES_SL_START_DATE }}')
+            GROUP BY
+                block_timestamp :: DATE
+        )
+        SELECT
+            block_date,
+            block_number
+        FROM
+            base
+        WHERE
+            block_date <> (
+                SELECT
+                    MAX(block_date)
+                FROM
+                    base
+            )
+    {% endset %}
 
-{% endset %}
+    {% set block_numbers_query %}
+        CREATE OR REPLACE TEMPORARY TABLE silver.erc20_balances_daily_block_numbers__intermediate_tmp AS
+        SELECT 
+            block_number,
+            block_date
+        FROM ({{ max_block_by_date_sql }})
+        WHERE block_date IN (
+            SELECT block_date 
+            FROM silver.erc20_balances_daily_block_dates__intermediate_tmp
+        )
+    {% endset %}
+    {% do run_query(block_numbers_query) %}
 
-{% set snapshot_rows_query %}
-create or replace temporary table silver.erc20_balances_snapshot_rows__intermediate_tmp as
-select 
-    block_date,
-    address,
-    contract_address
-from {{ ref("streamline__balances_erc20_daily_records") }} 
-where block_date = '{{ snapshot_date }}'
-except
-select
-    block_date,
-    address,
-    contract_address
-from {{ ref("streamline__balances_erc20_daily_complete") }}
-where block_date = '{{ snapshot_date }}'
+    {% set snapshot_rows_query %}
+        CREATE OR REPLACE TEMPORARY TABLE silver.erc20_balances_snapshot_rows__intermediate_tmp AS
+        SELECT 
+            block_date,
+            address,
+            contract_address
+        FROM {{ ref("streamline__balances_erc20_daily_records") }} 
+        WHERE block_date = '{{ snapshot_date }}'
+        EXCEPT
+        SELECT
+            block_date,
+            address,
+            contract_address
+        FROM {{ ref("streamline__balances_erc20_daily_complete") }}
+        WHERE block_date = '{{ snapshot_date }}'
+        LIMIT {{ vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT }}
+    {% endset %}
+    {% do run_query(snapshot_rows_query) %}
 
-limit {{ vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT }}
-
-{% endset %}
-{% do run_query(snapshot_rows_query) %}
-
-{% set daily_backfill_rows_query %}
-create or replace temporary table silver.erc20_balances_daily_backfill_rows__intermediate_tmp as
-select 
-    block_date,
-    address,
-    contract_address
-from {{ ref("streamline__balances_erc20_daily_records") }} 
-where block_date > '{{ snapshot_date }}'
-except
-select
-    block_date,
-    address,
-    contract_address
-from {{ ref("streamline__balances_erc20_daily_complete") }}
-where block_date > '{{ snapshot_date }}'
-
-limit {{ vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT }}
-{% endset %}
-{% do run_query(daily_backfill_rows_query) %}
+    {% set daily_backfill_rows_query %}
+        CREATE OR REPLACE TEMPORARY TABLE silver.erc20_balances_daily_backfill_rows__intermediate_tmp AS
+        SELECT 
+            block_date,
+            address,
+            contract_address
+        FROM {{ ref("streamline__balances_erc20_daily_records") }} 
+        WHERE block_date > '{{ snapshot_date }}'
+        EXCEPT
+        SELECT
+            block_date,
+            address,
+            contract_address
+        FROM {{ ref("streamline__balances_erc20_daily_complete") }}
+        WHERE block_date > '{{ snapshot_date }}'
+        LIMIT {{ vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT }}
+    {% endset %}
+    {% do run_query(daily_backfill_rows_query) %}
 
 {% endif %}
 
-
 WITH to_do AS (
-    select
+    SELECT
         block_date,
         address,
         contract_address
-    from
-       silver.erc20_balances_snapshot_rows__intermediate_tmp
-    union all
-    select
+    FROM silver.erc20_balances_snapshot_rows__intermediate_tmp
+    
+    UNION ALL
+    
+    SELECT
         block_date,
         address,
         contract_address
-    from
-        silver.erc20_balances_daily_backfill_rows__intermediate_tmp
-    order by block_date desc
+    FROM silver.erc20_balances_daily_backfill_rows__intermediate_tmp
+    
+    ORDER BY block_date DESC
     LIMIT {{ vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT }}
 )
+
 SELECT
     block_number,
-    DATE_PART('EPOCH_SECONDS', block_date) :: INT AS block_date_unix,
+    DATE_PART('EPOCH_SECONDS', block_date)::INT AS block_date_unix,
     address,
     contract_address,
-    DATE_PART('EPOCH_SECONDS', SYSDATE()::DATE) :: INT AS partition_key,
+    DATE_PART('EPOCH_SECONDS', SYSDATE()::DATE)::INT AS partition_key,
     live.udf_api(
         'POST',
         '{{ vars.GLOBAL_NODE_URL }}',
         OBJECT_CONSTRUCT(
-            'Content-Type',
-            'application/json',
-            'fsc-quantum-state',
-            'streamline'
+            'Content-Type', 'application/json',
+            'fsc-quantum-state', 'streamline'
         ),
         OBJECT_CONSTRUCT(
-            'id',
-            CONCAT(
+            'id', CONCAT(
                 contract_address,
                 '-',
                 address,
                 '-',
                 block_number
             ),
-            'jsonrpc',
-            '2.0',
-            'method',
-            'eth_call',
-            'params',
-            ARRAY_CONSTRUCT(
+            'jsonrpc', '2.0',
+            'method', 'eth_call',
+            'params', ARRAY_CONSTRUCT(
                 OBJECT_CONSTRUCT(
-                    'to',
-                    contract_address,
-                    'data',
-                    CONCAT(
+                    'to', contract_address,
+                    'data', CONCAT(
                         '0x70a08231000000000000000000000000',
-                        SUBSTR(
-                            address,
-                            3
-                        )
+                        SUBSTR(address, 3)
                     )
                 ),
                 utils.udf_int_to_hex(block_number)
@@ -149,9 +166,10 @@ SELECT
         ),
         '{{ vars.GLOBAL_NODE_VAULT_PATH }}'
     ) AS request
-FROM
-    to_do
-    join silver.erc20_balances_daily_block_numbers__intermediate_tmp using (block_date)
-ORDER BY partition_key DESC, block_number DESC
-
+FROM to_do
+JOIN silver.erc20_balances_daily_block_numbers__intermediate_tmp 
+    USING (block_date)
+ORDER BY 
+    partition_key DESC, 
+    block_number DESC
 LIMIT {{ vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT }}
