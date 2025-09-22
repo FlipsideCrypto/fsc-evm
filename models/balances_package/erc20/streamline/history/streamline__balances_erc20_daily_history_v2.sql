8d868a652514d818989aac6d2b708bd915cd6aef
Austin <austin@flipsidecrypto.com>
2025-09-22 16:22:13 -0400

    limit

--- models/balances_package/erc20/streamline/history/streamline__balances_erc20_daily_history_v2.sql
+++ models/balances_package/erc20/streamline/history/streamline__balances_erc20_daily_history_v2.sql
@@ -10,16 +10,6 @@
     tags = ['streamline','balances','history','erc20','phase_4']
 ) }}
 
---post_hook macro necessary for the streamline function call because model is materialized as a table, rather than a view
-
-
--- 1. create a query for snapshot vs daily backfill date
--- 2. create two temp tables, one for snapshot and one for daily backfill with n rows 
--- 3. union the two tables
--- 4. create the table with the unioned data with n rows, prioritize snapshot
--- 5. run the streamline function call on the table
-
-
 {% if execute %}
 
 {% set snapshot_date_query %}
@@ -64,7 +54,7 @@ select
     address,
     contract_address
 from {{ ref("streamline__balances_erc20_daily_records") }} 
-where block_date <> '{{ snapshot_date }}'
+where block_date > '{{ snapshot_date }}'
 except
 select
     block_number,
@@ -72,12 +62,13 @@ select
     address,
     contract_address
 from {{ ref("streamline__balances_erc20_daily_complete") }}
-where block_date <> '{{ snapshot_date }}'
+where block_date > '{{ snapshot_date }}'
 
 limit {{ vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT }}
 {% endset %}
 {% do run_query(daily_backfill_rows_query) %}
 
+{% endif %}
 
 
 WITH to_do AS (
