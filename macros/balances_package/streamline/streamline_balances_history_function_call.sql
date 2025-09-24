648ffc40d55382d96198ec43b810a1894ceb9889
drethereum <trevor.wenokur@gmail.com>
2025-09-24 12:18:33 -0600

    unioned view

--- macros/balances_package/streamline/streamline_balances_history_function_call.sql
+++ macros/balances_package/streamline/streamline_balances_history_function_call.sql
@@ -13,40 +13,42 @@
         {% set results = run_query(max_batch_query) %}
         {% set max_batch = results.columns[0].values()[0] %}
         
-        {# Process each batch sequentially #}
-        {% for batch_num in range(max_batch + 1) %}
-            {{ log("Processing batch " ~ batch_num ~ " of " ~ max_batch, info=True) }}
-            
-            {# Create a temporary view for this batch #}
-            {% set batch_view_name = this.identifier ~ '_batch_' ~ batch_num %}
-            {% set create_batch_view %}
-                CREATE OR REPLACE VIEW streamline.{{ batch_view_name }} AS
-                SELECT * FROM {{ this.schema }}.{{ this.identifier }}
+        {# Create a single view that unions all batches #}
+        {% set batch_view_name = this.identifier ~ '_batch' %}
+        {% set create_batch_view %}
+            CREATE OR REPLACE VIEW streamline.{{ batch_view_name }} AS
+            {% for batch_num in range(max_batch + 1) %}
+                {% if batch_num > 0 %}UNION ALL{% endif %}
+                SELECT
+                    *
+                FROM
+                    {{ this.schema }}.{{ this.identifier }}
                 WHERE batch = {{ batch_num }}
-            {% endset %}
-            {% do run_query(create_batch_view) %}
-            {{ log("Created view: streamline." ~ batch_view_name, info=True) }}
-            
-            {% set params = {
-                "external_table": 'balances_erc20',
-                "sql_limit": vars.BALANCES_SL_DAILY_HISTORY_BATCH_SIZE,
-                "producer_batch_size": vars.BALANCES_SL_ERC20_DAILY_HISTORY_PRODUCER_BATCH_SIZE,
-                "worker_batch_size": vars.BALANCES_SL_ERC20_DAILY_HISTORY_WORKER_BATCH_SIZE,
-                "async_concurrent_requests": vars.BALANCES_SL_ERC20_DAILY_HISTORY_ASYNC_CONCURRENT_REQUESTS,
-                "sql_source": 'balances_erc20_daily_history'
-            } %}
+            {% endfor %}
+        {% endset %}
+        {% do run_query(create_batch_view) %}
+        {{ log("Created batch view: streamline." ~ batch_view_name, info=True) }}
+        
+        {# Call the streamline function on the batch view #}
+        {% set params = {
+            "external_table": 'balances_erc20',
+            "sql_limit": vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT,
+            "producer_batch_size": vars.BALANCES_SL_ERC20_DAILY_HISTORY_PRODUCER_BATCH_SIZE,
+            "worker_batch_size": vars.BALANCES_SL_ERC20_DAILY_HISTORY_WORKER_BATCH_SIZE,
+            "async_concurrent_requests": vars.BALANCES_SL_ERC20_DAILY_HISTORY_ASYNC_CONCURRENT_REQUESTS,
+            "sql_source": batch_view_name
+        } %}
 
-            {% set function_call_sql %}
-            {{ fsc_utils.if_data_call_function_v2(
-                func = 'streamline.udf_bulk_rest_api_v2',
-                target = "streamline." ~ batch_view_name,
-                params = params
-            ) }}
-            {% endset %}
+        {% set function_call_sql %}
+        {{ fsc_utils.if_data_call_function_v2(
+            func = 'streamline.udf_bulk_rest_api_v2',
+            target = "streamline." ~ batch_view_name,
+            params = params
+        ) }}
+        {% endset %}
 
-            {% do run_query(function_call_sql) %}
-            {{ log("Completed batch " ~ batch_num, info=True) }}
-        {% endfor %}
+        {% do run_query(function_call_sql) %}
+        {{ log("Completed processing batch view", info=True) }}
     {% endif %}
 
 {% endmacro %}
