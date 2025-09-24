97795a0ebf089683a3878c889fb4fddad1f4a8a6
drethereum <trevor.wenokur@gmail.com>
2025-09-24 16:02:13 -0600

    revert rn logic in history tables

--- models/balances_package/native/streamline/history/streamline__balances_native_daily_history_requests.sql
+++ models/balances_package/native/streamline/history/streamline__balances_native_daily_history_requests.sql
@@ -19,98 +19,44 @@ WITH last_x_days AS (
         {{ ref("_max_block_by_date") }}
     WHERE block_date >= ('{{ vars.BALANCES_SL_START_DATE }}' :: TIMESTAMP) :: DATE
 ),
-to_do_snapshot AS (
-    SELECT
-        DISTINCT
-        block_date,
-        address
-    FROM
-        {{ ref("streamline__balances_native_daily_records") }}
-    WHERE
-        block_date = ('{{ vars.BALANCES_SL_START_DATE }}' :: TIMESTAMP) :: DATE
-    EXCEPT
-    SELECT
-        block_date,
-        address
-    FROM
-        {{ ref("streamline__balances_native_daily_complete") }}
-    WHERE block_date = ('{{ vars.BALANCES_SL_START_DATE }}' :: TIMESTAMP) :: DATE
-    LIMIT {{ vars.BALANCES_SL_NATIVE_DAILY_HISTORY_SQL_LIMIT }}
-),
-to_do_daily AS (
-    SELECT
-        DISTINCT
-        block_date,
-        address
-    FROM
-        {{ ref("streamline__balances_native_daily_records") }}
-    WHERE
-        block_date > ('{{ vars.BALANCES_SL_START_DATE }}' :: TIMESTAMP) :: DATE
-    EXCEPT
-    SELECT
-        block_date,
-        address
-    FROM
-        {{ ref("streamline__balances_native_daily_complete") }}
-    WHERE block_date > ('{{ vars.BALANCES_SL_START_DATE }}' :: TIMESTAMP) :: DATE
-    ORDER BY block_date DESC
-    LIMIT {{ vars.BALANCES_SL_ERC20_DAILY_HISTORY_SQL_LIMIT }}
-),
 to_do AS (
     SELECT
-        block_date,
-        'snapshot' AS TYPE,
-        address
-    FROM to_do_snapshot
-    UNION ALL
-    SELECT
-        block_date,
-        'daily' AS TYPE,
-        address
-    FROM to_do_daily
-),
-to_do_ranked AS (
+        DISTINCT
+        d.block_number,
+        d.block_date,
+        t.address
+    FROM
+        {{ ref("streamline__balances_native_daily_records") }} t
+        INNER JOIN last_x_days d 
+            ON t.block_date = d.block_date
+    --max daily block_number during the selected period, for each address
+    WHERE
+        t.block_date IS NOT NULL
+        AND d.block_date < (
+            SELECT MAX(block_date)
+            FROM last_x_days
+        ) 
+    EXCEPT
     SELECT
         block_number,
         block_date,
-        DATE_PART('EPOCH_SECONDS', block_date)::INT AS block_date_unix,
-        TYPE,
-        address,
-        IFF(
-            TYPE = 'snapshot',
-            DATE_PART('EPOCH_SECONDS', SYSDATE() :: DATE) :: INT,
-            ROUND(
-                block_number,
-                -3
-            )
-        ) AS partition_key,
-        ROW_NUMBER() OVER (ORDER BY block_number DESC) AS rn
+        address
     FROM
-        to_do
-        JOIN last_x_days USING (block_date)
-),
-to_do_batched AS (
-SELECT
-    block_number,
-    block_date,
-    block_date_unix,
-    TYPE,
-    address,
-    partition_key,
-    api_request,
-    rn
-FROM
-    to_do_ranked
-WHERE
-    rn <= {{ vars.BALANCES_SL_NATIVE_DAILY_HISTORY_SQL_LIMIT }}
-ORDER BY
-    block_date DESC --included for performance reasons
+        {{ ref("streamline__balances_native_daily_complete") }}
+    WHERE block_date IS NOT NULL
 )
 SELECT
     block_number,
-    block_date_unix,
+    DATE_PART('EPOCH_SECONDS', block_date) :: INT AS block_date_unix,
     address,
-    partition_key,
+    IFF(
+        block_date = ('{{ vars.BALANCES_SL_START_DATE }}' :: TIMESTAMP) :: DATE,
+        DATE_PART('EPOCH_SECONDS', SYSDATE() :: DATE) :: INT,
+        ROUND(
+            block_number,
+            -3
+        )
+    ) AS partition_key,
     OBJECT_CONSTRUCT(
         'data', OBJECT_CONSTRUCT(
             'id',
@@ -135,6 +81,7 @@ SELECT
         'url', '{{ vars.GLOBAL_NODE_URL }}'
     ) AS request
 FROM
-    to_do_batched
+    to_do
+ORDER BY partition_key DESC, block_number DESC
 
 LIMIT {{ vars.BALANCES_SL_NATIVE_DAILY_HISTORY_SQL_LIMIT }}
