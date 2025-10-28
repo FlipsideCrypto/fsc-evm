5cb55e97432b3f0dabbc46c975b461d5549755fb
drethereum <trevor.wenokur@gmail.com>
2025-10-28 10:09:11 -0600

    primitive specific curated defi recency vars

--- macros/curated_package/tests/curated_recency.sql
+++ macros/curated_package/tests/curated_recency.sql
@@ -1,7 +1,8 @@
 {% test curated_recency_defi(
     model,
     threshold_days=30,
-    percent_delta_threshold=10
+    percent_delta_threshold=10,
+    type=''
 ) %}
 
 {# Get variables #}
@@ -49,7 +50,15 @@ WITH source AS (
         WHERE
             (latest_timestamp < threshold_ts 
             OR (percent_delta < {{ percent_delta_threshold }} AND percent_delta <> 0))
-            AND platform NOT IN ('{{ vars.CURATED_DEFI_RECENCY_EXCLUSION_LIST | join("', '") }}')
+            {% if type == 'dex_swaps' %}
+            AND platform NOT IN ('{{ vars.CURATED_DEFI_DEX_SWAPS_RECENCY_EXCLUSION_LIST | join("', '") }}')
+            {% elif type == 'dex_lp_actions' %}
+            AND platform NOT IN ('{{ vars.CURATED_DEFI_DEX_LP_ACTIONS_RECENCY_EXCLUSION_LIST | join("', '") }}')
+            {% elif type == 'bridge' %}
+            AND platform NOT IN ('{{ vars.CURATED_DEFI_BRIDGE_RECENCY_EXCLUSION_LIST | join("', '") }}')
+            {% elif type == 'lending' %}
+            AND platform NOT IN ('{{ vars.CURATED_DEFI_LENDING_RECENCY_EXCLUSION_LIST | join("', '") }}')
+            {% endif %}
 -- failure to meet threshold requires manual review to determine if
 -- the protocol has newly deployed contracts, stale contracts, etc.
 -- if this is the case, the test should not apply to the relevant model(s)
