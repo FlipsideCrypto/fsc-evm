9406bfd20c1b3baa60f81a71124a7a28714a2dfa
drethereum <trevor.wenokur@gmail.com>
2025-11-05 09:47:35 -0700

    columns in silver

--- models/curated_package/defi/stablecoins/gold/defi__ez_stablecoins_supply.sql
+++ models/curated_package/defi/stablecoins/gold/defi__ez_stablecoins_supply.sql
@@ -24,7 +24,7 @@ SELECT
     amount_in_bridges,
     amount_in_dex_liquidity_pools,
     amount_in_lending_pools,
-    amount_in_all_contracts,
+    amount_in_contracts,
     inserted_timestamp,
     modified_timestamp,
     stablecoins_supply_complete_id AS ez_stablecoins_supply_id
