849cd8e67c97c8a5bd6adccba2dd2a22241955bd
drethereum <trevor.wenokur@gmail.com>
2025-11-07 12:14:27 -0700

    hardcode

--- models/curated_package/chain_stats/gold/chain_stats__ez_address_metrics.sql
+++ models/curated_package/chain_stats/gold/chain_stats__ez_address_metrics.sql
@@ -13,24 +13,4 @@
 SELECT
     *
 FROM
-{% if vars.GLOBAL_PROJECT_NAME == 'arbitrum' %}
-    {{ source('crosschain_chain_stats', 'ez_arbitrum_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'avalanche' %}
-    {{ source('crosschain_chain_stats', 'ez_avalanche_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'base' %}
-    {{ source('crosschain_chain_stats', 'ez_base_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'bob' %}
-    {{ source('crosschain_chain_stats', 'ez_bob_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'boba' %}
-    {{ source('crosschain_chain_stats', 'ez_boba_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'bsc' %}
-    {{ source('crosschain_chain_stats', 'ez_bsc_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'ethereum' %}
-    {{ source('crosschain_chain_stats', 'ez_ethereum_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'ink' %}
-    {{ source('crosschain_chain_stats', 'ez_ink_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'optimism' %}
-    {{ source('crosschain_chain_stats', 'ez_optimism_address_metrics') }}
-{% elif vars.GLOBAL_PROJECT_NAME == 'polygon' %}
-    {{ source('crosschain_chain_stats', 'ez_polygon_address_metrics') }}
-{% endif %}
+    crosschain.chain_stats.ez_{{ vars.GLOBAL_PROJECT_NAME }}_address_metrics
