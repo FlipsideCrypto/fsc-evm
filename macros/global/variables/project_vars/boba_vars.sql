a55069592da87db84d7dd3e94ca7f7df1e987a1f
sam <sam@flipsidecrypto.com>
2025-11-11 15:36:31 +0800

    add boba dex

--- macros/global/variables/project_vars/boba_vars.sql
+++ macros/global/variables/project_vars/boba_vars.sql
@@ -19,7 +19,23 @@
         'MAIN_GHA_SCHEDULED_SCORES_CRON': '25 5 * * *',
         'DECODER_SL_CONTRACT_ABIS_REALTIME_PRODUCER_BATCH_SIZE': 50,
         'DECODER_SL_CONTRACT_ABIS_REALTIME_WORKER_BATCH_SIZE': 50,
-        'MAIN_OBSERV_EXCLUSION_LIST_ENABLED': true
+        'MAIN_OBSERV_EXCLUSION_LIST_ENABLED': true,
+        'CURATED_DEFI_BRIDGE_RECENCY_EXCLUSION_LIST': ['meson-v1','symbiosis-v1'],
+        'CURATED_DEFI_DEX_SWAPS_CONTRACT_MAPPING': {
+            'uniswap': {
+                'v3': {
+                    'uni_v3_pool_created': '0xffcd7aed9c627e82a765c3247d562239507f6f1b'
+                }
+            },
+            'sushiswap': {
+                'v1': {
+                    'uni_v2_pair_created': '0xc35dadb65012ec5796536bd9864ed8773abc74c4'
+                },
+                'v2': {
+                    'uni_v3_pool_created': '0x0be808376ecb75a5cf9bb6d237d16cd37893d904'
+                }
+            }
+        }
     } %}
     
     {{ return(vars) }}
