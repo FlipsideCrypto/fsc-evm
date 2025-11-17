2db47031eb3f624e911ba734f2229599f1120703
drethereum <trevor.wenokur@gmail.com>
2025-11-17 16:55:57 -0700

    aave v1

---
+++ models/curated_package/contract_reads/streamline/records/aave/silver_reads__aave_v1_tvl.sql
@@ -0,0 +1,111 @@
+{# Get variables #}
+{% set vars = return_vars() %}
+{# Log configuration details #}
+{{ log_model_details() }}
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'uniswap_v3_tvl_id',
+    tags = ['silver','contract_reads','curated_daily']
+) }}
+
+WITH all_tokens AS (
+
+    SELECT
+        underlying_address AS contract_address,
+        protocol,
+        version,
+        CONCAT(
+            protocol,
+            '-',
+            version
+        ) AS platform
+    FROM
+        {{ ref('silver_lending__aave_ethereum_tokens') }} --relevant for ethereum only
+    WHERE
+        protocol = 'aave'
+        AND version = 'v1'
+
+{% if is_incremental() %}
+AND modified_timestamp > (
+    SELECT
+        MAX(modified_timestamp)
+    FROM
+        {{ this }}
+)
+{% endif %}
+UNION
+SELECT
+    underlying_address AS contract_address,
+    protocol,
+    version,
+    CONCAT(
+        protocol,
+        '-',
+        version
+    ) AS platform
+FROM
+    {{ ref('silver_lending__aave_tokens') }}
+WHERE
+    protocol = 'aave'
+    AND version = 'v1'
+
+{% if is_incremental() %}
+AND modified_timestamp > (
+    SELECT
+        MAX(modified_timestamp)
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+lending_pools AS (
+    SELECT
+        contract_address,
+        '0x3dfd23a6c5e8bbcfc9581d2e864a68feb6a076d3' AS address,
+        --Aave: LendingPoolCore
+        function_name,
+        function_sig,
+        input,
+        metadata,
+        protocol,
+        version,
+        platform
+    FROM
+        all_tokens
+    UNION ALL
+    SELECT
+        contract_address,
+        '0x1012cff81a1582ddd0616517efb97d02c5c17e25' AS address,
+        --Uniswap: LendingPoolCore in Aave v1 holds Uniswap v1 LP tokens as collateral
+        function_name,
+        function_sig,
+        input,
+        metadata,
+        protocol,
+        version,
+        platform
+    FROM
+        all_tokens
+)
+SELECT
+    contract_address,
+    address,
+    'balanceOf' AS function_name,
+    '0x70a08231' AS function_sig,
+    CONCAT(
+        '0x70a08231',
+        LPAD(SUBSTR(address, 3), 64, '0')
+    ) AS input,
+    NULL AS metadata,
+    protocol,
+    version,
+    platform,
+    {{ dbt_utils.generate_surrogate_key(
+        ['contract_address','address','input','platform']
+    ) }} AS uniswap_v3_tvl_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    lp_balances
