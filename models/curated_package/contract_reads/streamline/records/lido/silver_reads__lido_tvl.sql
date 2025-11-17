193acf191d2a08544516d111a95656d8714d601a
drethereum <trevor.wenokur@gmail.com>
2025-11-17 14:33:58 -0700

    lido

---
+++ models/curated_package/contract_reads/streamline/records/lido/silver_reads__lido_tvl.sql
@@ -0,0 +1,50 @@
+{# Get variables #}
+{% set vars = return_vars() %}
+
+{# Log configuration details #}
+{{ log_model_details() }}
+
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'lido_tvl_id',
+    tags = ['silver','contract_reads','curated_daily']
+) }}
+
+WITH pooled_assets AS (
+SELECT
+    '0xae7ab96520de3a18e5e111b5eaab095312d7fe84' AS contract_address,
+    NULL AS address,
+    'getTotalPooledEther' AS function_name,
+    '0x37cfdaca' AS function_sig,
+    RPAD(
+        function_sig,
+        64,
+        '0'
+    ) AS input
+UNION ALL
+SELECT
+    '0x9ee91f9f426fa633d227f7a9b000e28b9dfd8599' AS contract_address,
+    NULL AS address,
+    'getTotalPooledMatic' AS function_name,
+    '0xe00222a0' AS function_sig,
+    RPAD(
+        function_sig,
+        64,
+        '0'
+    ) AS input
+)
+
+SELECT
+    contract_address,
+    address,
+    function_name,
+    function_sig,
+    input,
+    {{ dbt_utils.generate_surrogate_key(
+        ['contract_address', 'address', 'function_name', 'function_sig', 'input']
+    ) }} AS lido_tvl_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM pooled_assets
---
+++ models/curated_package/contract_reads/streamline/records/streamline__contract_reads_daily_records.sql
@@ -0,0 +1,58 @@
+{# Get variables #}
+{% set vars = return_vars() %}
+
+{# Log configuration details #}
+{{ log_model_details() }}
+
+{# Set up dbt configuration #}
+{{ config (
+    materialized = "incremental",
+    unique_key = "contract_reads_daily_records_id",
+    cluster_by = "block_date",
+    full_refresh = vars.GLOBAL_STREAMLINE_FR_ENABLED,
+    tags = ['streamline','contract_reads','records','phase_4']
+) }}
+
+WITH lido_tvl AS (
+
+    SELECT
+        contract_address,
+        address,
+        function_name,
+        function_sig,
+        input
+    FROM
+        {{ ref('silver_reads__lido_tvl') }}
+
+{% if is_incremental() %}
+WHERE
+    modified_timestamp >= (
+        SELECT
+            MAX(modified_timestamp)
+        FROM
+            {{ this }}
+    )
+{% endif %}
+),
+all_records AS (
+    SELECT
+        *
+    FROM
+        lido_tvl
+)
+SELECT
+    contract_address,
+    address,
+    function_name,
+    function_sig,
+    input,
+    {{ dbt_utils.generate_surrogate_key(
+        ['contract_address', 'address', 'function_name', 'function_sig', 'input']
+    ) }} AS contract_reads_daily_records_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    all_records qualify (ROW_NUMBER() over (PARTITION BY contract_reads_daily_records_id
+ORDER BY
+    modified_timestamp DESC)) = 1
