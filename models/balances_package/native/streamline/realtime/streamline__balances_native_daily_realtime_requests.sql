7fccec822d417705edd602227bdcad4ec5b5ce86
Austin <austin@flipsidecrypto.com>
2025-11-17 14:25:51 -0500

    format

--- models/balances_package/native/streamline/realtime/streamline__balances_native_daily_realtime_requests.sql
+++ models/balances_package/native/streamline/realtime/streamline__balances_native_daily_realtime_requests.sql
@@ -17,7 +17,7 @@ WITH last_x_days AS (
         block_date
     FROM
         {{ ref("_max_block_by_date") }}
-        WHERE block_date >= DATEADD('day', {{ vars.BALANCES_DAILY_REALTIME_LOOKBACK_DAYS | default(-4) }}, SYSDATE())
+        WHERE block_date >= DATEADD('day', {{ vars.BALANCES_DAILY_REALTIME_LOOKBACK_DAYS }}, SYSDATE())
 ),
 to_do AS (
     SELECT
