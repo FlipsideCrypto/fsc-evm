654d6a26f62d9df2251c10a6bc02ff042c81d2a5
mattromano <mattromano88@gmail.com>
2025-12-10 10:40:56 -0800

    update incremental predicates for 1.10

--- macros/global/dbt/incremental_predicates.sql
+++ macros/global/dbt/incremental_predicates.sql
@@ -1,17 +1,11 @@
 {% macro standard_predicate(
         input_column = 'block_number'
     ) -%}
+    {%- set tmp_table_name = generate_alias_name(node = model) ~ '__dbt_tmp' -%}
     {%- set database_name = target.database -%}
-    {%- set schema_name = generate_schema_name(
-        node = model
-    ) -%}
-    {%- set table_name = generate_alias_name(
-        node = model
-    ) -%}
-    {%- set tmp_table_name = table_name ~ '__dbt_tmp' -%}
-    {%- set full_table_name = database_name ~ '.' ~ schema_name ~ '.' ~ table_name -%}
+    {%- set schema_name = generate_schema_name(node = model) -%}
     {%- set full_tmp_table_name = database_name ~ '.' ~ schema_name ~ '.' ~ tmp_table_name -%}
-    {{ full_table_name }}.{{ input_column }} >= (
+    DBT_INTERNAL_DEST.{{ input_column }} >= (
         SELECT
             MIN(
                 {{ input_column }}
