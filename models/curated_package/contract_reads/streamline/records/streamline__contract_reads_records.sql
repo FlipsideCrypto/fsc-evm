f0f0958c0f1e1eb19f6b768d961b187903ed2de5
drethereum <trevor.wenokur@gmail.com>
2025-12-12 10:54:58 -0700

    polymarket tvl

--- models/curated_package/contract_reads/streamline/records/streamline__contract_reads_records.sql
+++ models/curated_package/contract_reads/streamline/records/streamline__contract_reads_records.sql
@@ -16,6 +16,9 @@
 {% if vars.GLOBAL_PROJECT_NAME == 'ethereum' %}
     {% set _ = models.append(ref('silver_reads__lido_reads')) %}
 {% endif %}
+{% if vars.GLOBAL_PROJECT_NAME == 'polygon' %}
+    {% set _ = models.append(ref('silver_reads__polymarket_v1_reads')) %}
+{% endif %}
 {% set _ = models.append(ref('silver_reads__stablecoins_reads')) %}
 {% set _ = models.append(ref('silver_reads__uniswap_v2_reads')) %}
 {% set _ = models.append(ref('silver_reads__uniswap_v3_reads')) %}
