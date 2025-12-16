c11ad42cefbf4fd11e0c414fa239f1af32dcd611
mattromano <mattromano88@gmail.com>
2025-12-16 12:18:59 -0800

    update get merge

--- macros/global/dbt/get_merge_sql.sql
+++ macros/global/dbt/get_merge_sql.sql
@@ -1,8 +1,16 @@
 {% macro get_merge_sql(target, source, unique_key, dest_columns, incremental_predicates=none) -%}
     {% set predicate_override = "" %}
     {% if incremental_predicates and incremental_predicates|length > 0 and incremental_predicates[0] == "dynamic_range" %}
-        -- run some queries to dynamically determine the min + max of this 'date_column' in the new data
-        {% set predicate_override = fsc_utils.dynamic_range_predicate(source, incremental_predicates[1], "DBT_INTERNAL_DEST") %}
+        {#
+            Generate a runtime predicate using subqueries instead of run_query().
+            This avoids the dbt-snowflake 1.10 issue where the tmp table doesn't exist
+            at macro compilation time.
+
+            The predicate limits the destination table scan to only rows where the
+            predicate column falls within the min/max range of the source data.
+        #}
+        {% set predicate_column = incremental_predicates[1] %}
+        {% set predicate_override = 'DBT_INTERNAL_DEST.' ~ predicate_column ~ ' >= (SELECT MIN(' ~ predicate_column ~ ') FROM ' ~ source ~ ') AND DBT_INTERNAL_DEST.' ~ predicate_column ~ ' <= (SELECT MAX(' ~ predicate_column ~ ') FROM ' ~ source ~ ')' %}
     {% endif %}
     {% set predicates = [predicate_override] if predicate_override else incremental_predicates %}
     -- standard merge from here - dispatch to adapter's get_merge_sql
