a32a98e6443d425578d000be3fc03b4b90427194
mattromano <mattromano88@gmail.com>
2025-12-16 15:45:13 -0800

    revert and fix predicates alias

--- macros/global/dbt/incremental_predicates.sql
+++ macros/global/dbt/incremental_predicates.sql
@@ -1,16 +1,22 @@
 {% macro standard_predicate(
         input_column = 'block_number'
     ) -%}
-    {#
-        NOTE: This macro returns a signal value that get_merge_sql will detect and strip.
-
-        In dbt-snowflake 1.10+, the old fsc_utils.dynamic_range_predicate() approach
-        doesn't work because run_query() can't access the tmp view at compile time.
-
-        The get_merge_sql override detects this signal and strips the predicate,
-        allowing the merge to proceed without partition pruning.
-
-        See: https://github.com/dbt-labs/dbt-snowflake/pull/93
-    #}
-    standard_predicate:{{ input_column }}
+    {%- set database_name = target.database -%}
+    {%- set schema_name = generate_schema_name(
+        node = model
+    ) -%}
+    {%- set table_name = generate_alias_name(
+        node = model
+    ) -%}
+    {%- set tmp_table_name = table_name ~ '__dbt_tmp' -%}
+    {%- set full_table_name = database_name ~ '.' ~ schema_name ~ '.' ~ table_name -%}
+    {%- set full_tmp_table_name = database_name ~ '.' ~ schema_name ~ '.' ~ tmp_table_name -%}
+    DBT_INTERNAL_DEST.{{ input_column }} >= (
+        SELECT
+            MIN(
+                {{ input_column }}
+            )
+        FROM
+            {{ full_tmp_table_name }}
+    )
 {%- endmacro %}
