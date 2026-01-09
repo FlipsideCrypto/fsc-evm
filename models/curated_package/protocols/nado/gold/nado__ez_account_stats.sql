712471305d0e15000188cf702d62d58385b31f87
mattromano <mattromano88@gmail.com>
2026-01-09 10:33:00 -0800

    configure for nado ink

---
+++ models/curated_package/protocols/nado/gold/nado__ez_account_stats.sql
@@ -0,0 +1,53 @@
+{# Log configuration details #}
+{{ log_model_details() }}
+
+{{ config(
+    materialized = 'view',
+    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'CLOB, DEX, STATS',
+    } } },
+    tags = ['gold','nado','curated']
+) }}
+
+SELECT
+    subaccount,
+    trader,
+    first_trade_timestamp,
+    last_trade_timestamp,
+    account_age,
+    trade_count,
+    DENSE_RANK() over (
+        ORDER BY
+            trade_count DESC
+    ) AS trade_count_rank,
+    trade_count_24h,
+    DENSE_RANK() over (
+        ORDER BY
+            trade_count_24h DESC
+    ) AS trade_count_rank_24h,
+    perp_trade_count,
+    spot_trade_count,
+    long_count,
+    short_count,
+    total_usd_volume,
+    DENSE_RANK() over (
+        ORDER BY
+            total_usd_volume DESC
+    ) AS total_usd_volume_rank,
+    total_usd_volume_24h,
+    DENSE_RANK() over (
+        ORDER BY
+            total_usd_volume_24h DESC
+    ) AS total_usd_volume_rank_24h,
+    avg_usd_trade_size,
+    total_fee_amount,
+    total_base_delta_amount,
+    total_quote_delta_amount,
+    total_liquidation_amount,
+    total_liquidation_amount_quote,
+    total_liquidation_count,
+    nado_account_id as ez_account_stats_id,
+    inserted_timestamp,
+    modified_timestamp
+FROM
+    {{ ref('silver__nado_account_stats') }}
+ORDER BY total_usd_volume_rank DESC
---
+++ models/curated_package/protocols/nado/gold/nado__ez_spot_trades.sql
@@ -0,0 +1,56 @@
+{# Log configuration details #}
+{{ log_model_details() }}
+
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'ez_spot_trades_id',
+    merge_exclude_columns = ["inserted_timestamp"],
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(tx_hash,symbol,trader,digest,subaccount), SUBSTRING(symbol,trader)",
+    tags = ['gold','nado','curated']
+) }}
+
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    contract_address,
+    event_name,
+    event_index,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    symbol,
+    digest,
+    trader,
+    subaccount,
+    trade_type,
+    order_type,
+    market_reduce_flag,
+    expiration,
+    nonce,
+    is_taker,
+    price_amount_unadj,
+    price_amount,
+    amount_unadj,
+    amount,
+    amount_usd,
+    fee_amount_unadj,
+    fee_amount,
+    base_delta_amount_unadj,
+    base_delta_amount,
+    quote_delta_amount_unadj,
+    quote_delta_amount,
+    nado_spot_id AS ez_spot_trades_id,
+    inserted_timestamp,
+    modified_timestamp
+FROM
+    {{ ref('silver__nado_spot') }}
+{% if is_incremental() %}
+WHERE
+    modified_timestamp > (
+        SELECT
+            MAX(modified_timestamp)
+        FROM
+            {{ this }}
+    )
+{% endif %}
