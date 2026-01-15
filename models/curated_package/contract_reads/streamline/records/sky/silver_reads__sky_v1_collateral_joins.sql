2018e699358feba35b135d9e4aa5381790459896
drethereum <trevor.wenokur@gmail.com>
2026-01-14 17:24:54 -0700

    refactor

--- models/curated_package/contract_reads/streamline/records/sky/silver_reads__sky_v1_collateral_joins.sql
+++ models/curated_package/contract_reads/streamline/records/sky/silver_reads__sky_v1_collateral_joins.sql
@@ -12,11 +12,13 @@
 ) }}
 
 -- Sky Protocol (MakerDAO) collateral join discovery
--- Discovers join contracts from MCD_VAT rely events and maps to underlying tokens via gem() calls
+-- Discovers join contracts from MCD_VAT rely events via gem() calls
 
 WITH rely_events AS (
-    SELECT DISTINCT
-        LOWER(CONCAT('0x', SUBSTR(topic_1, -40))) AS join_address
+    SELECT
+        DISTINCT
+        LOWER(CONCAT('0x', SUBSTR(topic_1, -40))) AS join_address,
+        block_number
     FROM
         {{ ref('core__fact_event_logs') }}
     WHERE
@@ -26,79 +28,36 @@ WITH rely_events AS (
         {% if is_incremental() %}
         AND modified_timestamp > (SELECT MAX(modified_timestamp) FROM {{ this }})
         {% endif %}
-),
-
-gem_calls AS (
-    SELECT
-        join_address,
-        live.udf_api(
-            'POST',
-            '{URL}',
-            OBJECT_CONSTRUCT(
-                'Content-Type', 'application/json',
-                'fsc-quantum-state', 'livequery'
-            ),
-            utils.udf_json_rpc_call(
-                'eth_call',
-                [
-                    {
-                        'to': join_address,
-                        'from': null,
-                        'data': '0x7bd2bea7'
-                    },
-                    'latest'
-                ],
-                concat_ws('-', join_address, 'gem')
-            ),
-            '{{ vars.GLOBAL_NODE_VAULT_PATH }}'
-        ) AS api_response
-    FROM
-        rely_events
-),
-
-parsed_joins AS (
-    SELECT
-        join_address,
-        api_response:data:result::STRING AS result_hex,
-        CASE
-            WHEN result_hex IS NOT NULL
-            AND LENGTH(result_hex) >= 42
-            AND result_hex != '0x'
-            AND result_hex != '0x0000000000000000000000000000000000000000000000000000000000000000'
-            THEN LOWER(CONCAT('0x', SUBSTR(result_hex, -40)))
-            ELSE NULL
-        END AS token_address
-    FROM
-        gem_calls
-    WHERE
-        api_response:data:result IS NOT NULL
-        AND api_response:data:result::STRING != '0x'
-)
-
-final AS (
-    SELECT
-        p.join_address,
-        p.token_address
-    FROM
-        parsed_joins p
-    WHERE
-        p.token_address IS NOT NULL
-        AND p.token_address != '0x0000000000000000000000000000000000000000'
-UNION ALL
-SELECT
-    '0x37305b1cd40574e4c5ce33f8e8306be057fd7341' AS join_address, -- Sky: PSM
-    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' AS token_address, -- USDC
 )
 
 SELECT
     join_address,
-    token_address,
-    'sky' AS protocol,
-    'v1' AS version,
-    CONCAT(protocol, '-', version) AS platform,
+    OBJECT_CONSTRUCT(
+        'id', CONCAT(join_address, '-gem'),
+        'jsonrpc', '2.0',
+        'method', 'eth_call',
+        'params', ARRAY_CONSTRUCT(
+            OBJECT_CONSTRUCT(
+                'to', join_address,
+                'data', '0x7bd2bea7'
+            ),
+            utils.udf_int_to_hex(block_number)
+        )
+    ) AS rpc_request,
+    live.udf_api(
+        'POST',
+        '{{ vars.GLOBAL_NODE_URL }}',
+        OBJECT_CONSTRUCT(
+            'Content-Type', 'application/json',
+            'fsc-quantum-state', 'livequery'
+        ),
+        rpc_request,
+        '{{ vars.GLOBAL_NODE_VAULT_PATH }}'
+    ) AS response,
+    response:data:result::STRING AS result_hex,
     {{ dbt_utils.generate_surrogate_key(['join_address']) }} AS sky_v1_collateral_joins_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
-    final
+    rely_events
--- models/curated_package/contract_reads/streamline/records/sky/silver_reads__sky_v1_reads.sql
+++ models/curated_package/contract_reads/streamline/records/sky/silver_reads__sky_v1_reads.sql
@@ -13,37 +13,36 @@
 
 WITH collateral_joins AS (
     SELECT
-        token_address AS contract_address,
-        join_address AS address,
-        protocol,
-        version,
-        platform
+        join_address,
+        LOWER(CONCAT('0x', SUBSTR(result_hex, -40))) AS token_address
     FROM
         {{ ref('silver_reads__sky_v1_collateral_joins') }}
-
-{% if is_incremental() %}
-WHERE modified_timestamp > (
+    WHERE
+        result_hex IS NOT NULL
+        AND LENGTH(result_hex) >= 42
+        AND result_hex NOT IN ('0x', '0x0000000000000000000000000000000000000000000000000000000000000000')
+    {% if is_incremental() %}
+        AND modified_timestamp > (SELECT MAX(modified_timestamp) FROM {{ this }})
+    {% endif %}
+    UNION ALL
     SELECT
-        MAX(modified_timestamp)
-    FROM
-        {{ this }}
-)
-{% endif %}
+        '0x37305b1cd40574e4c5ce33f8e8306be057fd7341' AS join_address, -- Sky: PSM
+        '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' AS token_address  -- USDC
 )
 
 SELECT
-    contract_address,
-    address,
+    token_address AS contract_address,
+    join_address AS address,
     'balanceOf' AS function_name,
     '0x70a08231' AS function_sig,
     CONCAT(
         function_sig,
-        LPAD(SUBSTR(address, 3), 64, '0')
+        LPAD(SUBSTR(join_address, 3), 64, '0')
     ) AS input,
     NULL::VARIANT AS metadata,
-    protocol,
-    version,
-    platform,
+    'sky' AS protocol,
+    'v1' AS version,
+    CONCAT(protocol, '-', version) AS platform,
     {{ dbt_utils.generate_surrogate_key(
         ['contract_address','address','input','platform']
     ) }} AS sky_v1_reads_id,
@@ -52,3 +51,6 @@ SELECT
     '{{ invocation_id }}' AS _invocation_id
 FROM
     collateral_joins
+WHERE
+    token_address IS NOT NULL
+    AND token_address != '0x0000000000000000000000000000000000000000'
